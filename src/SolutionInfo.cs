8d83d9e9bcee59a67fea0cf7e8dea74bb7968f14
Andre Hofmeister <sw.padme.amidala@icloud.com>
2019-03-11 19:47:38 +0100

    Prepared new release cycle.

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyProduct(".NET Testcontainers")]
-[assembly: AssemblyVersion("0.0.5")]
-[assembly: AssemblyFileVersion("0.0.5")]
+[assembly: AssemblyVersion("0.0.6")]
+[assembly: AssemblyFileVersion("0.0.6")]
 [assembly: AssemblyCopyright("Copyright (c) Andre Hofmeister")]
 
 [assembly: InternalsVisibleTo("DotNet.Testcontainers.Tests")]
