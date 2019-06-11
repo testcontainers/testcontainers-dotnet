9d829b23fe8dcbe54bb4c486e1ee750466f2fa7a
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2019-06-11 19:09:00 +0200

    Added code of conduct, closes #80.

--- src/DotNet.Testcontainers.Tests/Fixtures/ParseDockerImageFixture.cs
+++ src/DotNet.Testcontainers.Tests/Fixtures/ParseDockerImageFixture.cs
@@ -9,6 +9,7 @@ namespace DotNet.Testcontainers.Tests.Fixtures
     {
       this.Add(new ParseDockerImageFixtureSerializable(new TestcontainersImage("baz/foo", "bar", "1.0.0")), "baz/foo/bar:1.0.0");
       this.Add(new ParseDockerImageFixtureSerializable(new TestcontainersImage("foo", "bar", "1.0.0")), "foo/bar:1.0.0");
+      this.Add(new ParseDockerImageFixtureSerializable(new TestcontainersImage("foo", "bar", string.Empty)), "foo/bar");
       this.Add(new ParseDockerImageFixtureSerializable(new TestcontainersImage("foo", "bar", string.Empty)), "foo/bar:latest");
       this.Add(new ParseDockerImageFixtureSerializable(new TestcontainersImage(string.Empty, "bar", "1.0.0")), "bar:1.0.0");
       this.Add(new ParseDockerImageFixtureSerializable(new TestcontainersImage(string.Empty, "bar", string.Empty)), "bar:latest");
--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyProduct(".NET Testcontainers")]
-[assembly: AssemblyVersion("0.0.7")]
-[assembly: AssemblyFileVersion("0.0.7")]
+[assembly: AssemblyVersion("0.0.8")]
+[assembly: AssemblyFileVersion("0.0.8")]
 [assembly: AssemblyCopyright("Copyright (c) Andre Hofmeister")]
 
 [assembly: InternalsVisibleTo("DotNet.Testcontainers.Tests")]
