399fb88cc114c139ec8a209edc2c1a68b99d44f3
Joana Deluca Kleis <joana.deluca@philips.com>
2020-01-07 18:08:07 -0300

    [178] #BUGFIX 'assemblyName: DotNet.Testcontainers; function WaitUntilPortsAreAvailable' {Added multiple commands to verify port availability}

--- src/DotNet.Testcontainers/Containers/WaitStrategies/WaitUntilPortsAreAvailable.cs
+++ src/DotNet.Testcontainers/Containers/WaitStrategies/WaitUntilPortsAreAvailable.cs
@@ -5,7 +5,7 @@ namespace DotNet.Testcontainers.Containers.WaitStrategies
   internal class WaitUntilPortsAreAvailable : WaitUntilBashCommandsAreCompleted
   {
     public WaitUntilPortsAreAvailable(params int[] ports) :
-      base(ports.Select(port => $"timeout 15 bash -c \"echo > /dev/tcp/localhost/{port}\"").ToArray())
+      base(ports.Select(port => $"true && (cat /proc/net/tcp{{,6}} | awk '{{print $2}}' | grep -i :{port} || nc -vz -w 1 localhost {port} || /bin/bash -c '</dev/tcp/localhost/{port}')").ToArray())
     {
     }
   }
