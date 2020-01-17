5bfea25b9ac61fe126e51dfc013fff037e18f748
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2020-01-17 17:20:03 +0100

    #INIT 'assemblyName: DotNet.Testcontainers; version: 1.2.0'

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyProduct(".NET Testcontainers")]
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.0")]
 [assembly: AssemblyCopyright("Copyright (c) Andre Hofmeister")]
 
 [assembly: InternalsVisibleTo("DotNet.Testcontainers.Tests")]
