967fc69bfd76667fec78f6385acd1cd023cc1faf
Anton Evdakov <aevdakov@AM.IDTCORP.NET>
2020-03-20 16:17:45 +0300

    [220] #IMPLEMENT 'assemblyName: DotNet.Testcontainers; function: OracleDbFixture, OracleDbTestcontainerTest, OracleTestContainer, OracleTestContainer' {Add Oracle database support}

---
+++ src/DotNet.Testcontainers.Tests/Unit/Containers/Unix/Database/OracleDbTestcontainerTest.cs
@@ -0,0 +1,25 @@
+﻿namespace DotNet.Testcontainers.Tests.Unit.Containers.Unix.Database
+{
+  using Fixtures;
+  using Xunit;
+
+  public class OracleDbTestcontainerTest : IClassFixture<OracleDbFixture>
+  {
+    private readonly OracleDbFixture oracleDbFixture;
+
+    public OracleDbTestcontainerTest(OracleDbFixture oracleDbFixture)
+    {
+      this.oracleDbFixture = oracleDbFixture;
+    }
+
+    [Fact]
+    public void CanQueryContainerUsingProvidedConnectionString()
+    {
+      // Act
+      var connectionString = this.oracleDbFixture.OracleTestContainer.ConnectionString;
+
+      // Assert
+      Assert.NotNull(connectionString);
+    }
+  }
+}
---
+++ src/DotNet.Testcontainers/Containers/Configurations/Databases/OracleDbTestcontainerConfiguration.cs
@@ -0,0 +1,34 @@
+﻿namespace DotNet.Testcontainers.Containers.Configurations.Databases
+{
+  using DotNet.Testcontainers.Containers.Configurations.Abstractions;
+  using DotNet.Testcontainers.Containers.WaitStrategies;
+
+  public sealed class OracleTestontainerConfiguration : TestcontainerDatabaseConfiguration
+  {
+    public OracleTestontainerConfiguration() : base("wnameless/oracle-xe-11g-r2", 1521)
+    {
+      this.Environments["ORACLE_ALLOW_EMPTY_PASSWORD"] = "yes";
+    }
+
+    public override string Database
+    {
+      get => this.Environments["ORACLE_DATABASE"];
+      set => this.Environments["ORACLE_DATABASE"] = value;
+    }
+
+    public override string Username
+    {
+      get => this.Environments["ORACLE_USER"];
+      set => this.Environments["ORACLE_USER"] = value;
+    }
+
+    public override string Password
+    {
+      get => this.Environments["ORACLE_PASSWORD"];
+      set => this.Environments["ORACLE_PASSWORD"] = value;
+    }
+
+    public override IWaitForContainerOS WaitStrategy => Wait.ForUnixContainer()
+      .UntilPortIsAvailable(this.DefaultPort);
+  }
+}
