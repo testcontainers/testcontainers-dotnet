9fa507789039ad2d1cd07eca162c370eaf785245
mustafa.aydin <mustafaonuraydin@gmail.com>
2020-07-18 17:58:33 +0300

    [#258] #IMPLEMENT 'assemblyName: DotNet.Testcontainers; function: CouchbaseTestcontainer' {Add pre-configured Couchbase Testcontainer.}

---
+++ src/DotNet.Testcontainers.Tests/Unit/Containers/Unix/Database/CouchbaseTestcontainerTest.cs
@@ -0,0 +1,179 @@
+namespace DotNet.Testcontainers.Tests.Unit.Containers.Unix.Database
+{
+  using System;
+  using System.Threading.Tasks;
+  using Couchbase;
+  using Testcontainers.Containers.Builders;
+  using Testcontainers.Containers.Configurations.Databases;
+  using Testcontainers.Containers.Modules.Databases;
+  using Xunit;
+
+  public class CouchbaseTestcontainerTest
+  {
+
+    [Fact]
+    public async Task ConnectionEstablished()
+    {
+      //Given
+      var couchbaseTestcontainer = new TestcontainersBuilder<CouchbaseTestcontainer>()
+        .WithDatabase(new CouchbaseTestcontainerConfiguration("Administrator", "password", "dotnet-testcontainers"))
+        .WithExposedPort(8091)
+        .WithExposedPort(11210)
+        .WithPortBinding(8091, 8091)
+        .WithPortBinding(11210, 11210)
+        .Build();
+
+      await couchbaseTestcontainer.StartAsync();
+
+      //When
+      var cluster = await Cluster.ConnectAsync(couchbaseTestcontainer.Hostname, "Administrator","password");
+      var bucket = await cluster.BucketAsync("dotnet-testcontainers");
+      var pingReport = await bucket.PingAsync();
+
+      //Then
+      Assert.NotNull(pingReport);
+      await couchbaseTestcontainer.StopAsync();
+    }
+
+    [Fact]
+    public async Task CrudWithRecentlyCreatedBucket()
+    {
+      //Given
+      var couchbaseTestcontainer = new TestcontainersBuilder<CouchbaseTestcontainer>()
+        .WithDatabase(new CouchbaseTestcontainerConfiguration("Administrator", "password", "dotnet-testcontainers"))
+        .WithExposedPort(8091)
+        .WithExposedPort(11210)
+        .WithPortBinding(8091, 8091)
+        .WithPortBinding(11210, 11210)
+        .Build();
+
+      await couchbaseTestcontainer.StartAsync();
+
+      await couchbaseTestcontainer.CreateBucket("CRUD");
+
+      //When & Then
+      var cluster = await Cluster.ConnectAsync(couchbaseTestcontainer.Hostname, "Administrator","password");
+      var bucket = await cluster.BucketAsync("CRUD");
+      var collection = bucket.DefaultCollection();
+
+      // Create
+      await collection.InsertAsync(
+        "customer-id-1",
+        new Customer {Name = "Mustafa", Age = 29}
+        );
+
+      // Read
+      var customer = (await collection.GetAsync("customer-id-1")).ContentAs<Customer>();
+      Assert.True(customer.Name == "Mustafa");
+      Assert.True(customer.Age == 29);
+
+      //Update
+      customer.Age = 30;
+      customer.Name = "Onur";
+      await collection.UpsertAsync("customer-id-1", customer);
+
+      var updatedCustomer = (await collection.GetAsync("customer-id-1")).ContentAs<Customer>();
+      Assert.True(updatedCustomer.Name == "Onur");
+      Assert.True(updatedCustomer.Age == 30);
+
+      //Delete
+      await collection.RemoveAsync("customer-id-1");
+      var existsResult = await collection.ExistsAsync("customer-id-1");
+      Assert.False(existsResult.Exists);
+      await couchbaseTestcontainer.StopAsync();
+    }
+
+    private class Customer
+    {
+      public string Name { get; set; }
+      public int Age{ get; set; }
+    }
+
+    [Fact]
+    public void ShouldNotThrowsArgumentOutOfRangeException_WhenClusterConfigurationIsValid()
+    {
+      //Given
+      var configuration = new CouchbaseTestcontainerConfiguration("Administrator", "password", "dotnet-testcontainers");
+
+      //When & Then
+      Func<CouchbaseTestcontainerConfiguration> action = () => configuration
+        .ClusterRamSize(1024)
+        .ClusterIndexRamSize(512)
+        .ClusterEventingRamSize(256)
+        .ClusterFtsRamSize(256)
+        .ClusterAnalyticsRamSize(1024);;
+    }
+
+    [Fact]
+    public void ShouldThrowsArgumentOutOfRangeException_WhenClusterRamSizeIsNotValid()
+    {
+      //Given
+      var configuration = new CouchbaseTestcontainerConfiguration("Administrator", "password", "test");
+
+      //When
+      Func<CouchbaseTestcontainerConfiguration> action = () => configuration.ClusterRamSize(1023);
+
+      //Then
+      var exception = Assert.Throws<ArgumentOutOfRangeException>(action);
+      Assert.True(exception.Message == "Couchbase clusterRamSize ram size can not be less than 1024 MB. (Parameter 'clusterRamSize')");
+    }
+
+    [Fact]
+    public void ShouldThrowsArgumentOutOfRangeException_WhenClusterAnalyticsRamSizeIsNotValid()
+    {
+      //Given
+      var configuration = new CouchbaseTestcontainerConfiguration("Administrator", "password", "test");
+
+      //When
+      Func<CouchbaseTestcontainerConfiguration> action = () => configuration.ClusterAnalyticsRamSize(1023);
+
+      //Then
+      var exception = Assert.Throws<ArgumentOutOfRangeException>(action);
+      Assert.True(exception.Message == "Couchbase clusterAnalyticsRamSize ram size can not be less than 1024 MB. (Parameter 'clusterAnalyticsRamSize')");
+    }
+
+    [Fact]
+    public void ShouldThrowsArgumentOutOfRangeException_WhenClusterEventingRamSizeIsNotValid()
+    {
+      //Given
+      var configuration = new CouchbaseTestcontainerConfiguration("Administrator", "password", "test");
+
+      //When
+      Func<CouchbaseTestcontainerConfiguration> action = () => configuration.ClusterEventingRamSize(255);
+
+      //Then
+      var exception = Assert.Throws<ArgumentOutOfRangeException>(action);
+      Assert.True(exception.Message == "Couchbase clusterEventingRamSize ram size can not be less than 256 MB. (Parameter 'clusterEventingRamSize')");
+    }
+
+    [Fact]
+    public void ShouldThrowsArgumentOutOfRangeException_WhenClusterFtsRamSizeIsNotValid()
+    {
+      //Given
+      var configuration = new CouchbaseTestcontainerConfiguration("Administrator", "password", "test");
+
+      //When
+      Func<CouchbaseTestcontainerConfiguration> action = () => configuration.ClusterFtsRamSize(255);
+
+      //Then
+      var exception = Assert.Throws<ArgumentOutOfRangeException>(action);
+      Assert.True(exception.Message == "Couchbase clusterFtsRamSize ram size can not be less than 256 MB. (Parameter 'clusterFtsRamSize')");
+    }
+
+    [Fact]
+    public void ShouldThrowsArgumentOutOfRangeException_WhenClusterIndexRamSizeIsNotValid()
+    {
+      //Given
+      var configuration = new CouchbaseTestcontainerConfiguration("Administrator", "password", "test");
+
+      //When
+      Func<CouchbaseTestcontainerConfiguration> action = () => configuration.ClusterIndexRamSize(511);
+
+      //Then
+      var exception = Assert.Throws<ArgumentOutOfRangeException>(action);
+      Assert.True(exception.Message == "Couchbase clusterIndexRamSize ram size can not be less than 512 MB. (Parameter 'clusterIndexRamSize')");
+    }
+
+  }
+
+}
