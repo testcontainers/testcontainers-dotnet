ae59480ad166baa4ad40b1f51473ec23d67a4538
Mike Eshva <reg@eshva.ru>
2021-02-10 20:34:36 +0300

    a comment fix and better hostname for a running container

--- src/DotNet.Testcontainers/Containers/Modules/TestcontainersContainer.cs
+++ src/DotNet.Testcontainers/Containers/Modules/TestcontainersContainer.cs
@@ -93,7 +93,7 @@ namespace DotNet.Testcontainers.Containers.Modules
               return "localhost";
             }
           default:
-            return this.IpAddress;
+            return this.configuration.Hostname;
         }
       }
     }
