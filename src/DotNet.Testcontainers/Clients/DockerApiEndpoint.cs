943e6c4cc0158c1e62ab2289d5bd2891f13f99b5
Peter Kurfer <peter.kurfer@googlemail.com>
2021-03-08 18:28:00 +0100

    Prepare cert auth setup based on env variables

--- src/DotNet.Testcontainers/Clients/DockerApiEndpoint.cs
+++ src/DotNet.Testcontainers/Clients/DockerApiEndpoint.cs
@@ -1,13 +1,54 @@
 namespace DotNet.Testcontainers.Clients
 {
   using System;
+  using System.IO;
+  using System.Linq;
   using Docker.DotNet;
-  using DotNet.Testcontainers.Services;
+  using Docker.DotNet.X509;
+  using Services;
 
   internal static class DockerApiEndpoint
   {
+    private const string DockerTlsVerifyEnvName = "DOCKER_TLS_VERIFY";
+    private const string DockerCertPathEnvName = "DOCKER_CERT_PATH";
+
+    private static readonly string[] RequiredCertFiles = { "ca.pem", "cert.pem", "key.pem" };
+
     public static Uri Default => TestcontainersHostService.GetService<IOperatingSystem>().DockerApiEndpoint;
 
-    public static Credentials DefaultCredentials => new AnonymousCredentials();
+    public static Credentials DefaultCredentials => DetermineCredentialsFromEnv();
+
+    internal static Credentials DetermineCredentialsFromEnv() => DetermineCredentials(
+      Environment.GetEnvironmentVariable(DockerTlsVerifyEnvName),
+      Environment.GetEnvironmentVariable(DockerCertPathEnvName)
+    );
+
+    internal static Credentials DetermineCredentials(string tlsVerify, string certPath)
+    {
+      // check if TLS_VERIFY is set and enabled otherwise don't authenticate at all
+      var anonymous = new AnonymousCredentials();
+      if (string.IsNullOrEmpty(tlsVerify) || !(bool.TryParse(tlsVerify, out var tlsEnabled) && tlsEnabled))
+      {
+        return anonymous;
+      }
+
+      // if no path is set - it's assumed there are not certs
+      if (string.IsNullOrEmpty(certPath))
+      {
+        return anonymous;
+      }
+
+      var allRequireFilesPresent = RequiredCertFiles
+        .Select(f => Path.Combine(certPath, f))
+        .All(File.Exists);
+
+      if (!allRequireFilesPresent)
+      {
+        return anonymous;
+      }
+
+      //TODO read certs
+      return new CertificateCredentials(null);
+    }
   }
 }
