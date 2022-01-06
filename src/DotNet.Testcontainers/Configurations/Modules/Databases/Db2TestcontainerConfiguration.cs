bb99a7e03680183d587d71cb25cad9b967a060bb
Anders Meidahl <mns@danskebank.dk>
2022-01-06 17:13:38 +0100

    Added DB2 Database

---
+++ src/DotNet.Testcontainers/Configurations/Modules/Databases/Db2TestcontainerConfiguration.cs
@@ -0,0 +1,62 @@
+namespace DotNet.Testcontainers.Configurations
+{
+  using System;
+  using System.IO;
+  using DotNet.Testcontainers.Builders;
+
+  public class Db2TestcontainerConfiguration : TestcontainerDatabaseConfiguration
+  {
+    private const string Db2Image = "remote-docker-hub.artifactory.danskenet.net/ibmcom/db2:11.5.7.0";
+
+    private const int Db2Port = 50000;
+    private const string WaitUntilMessageIsLogged = "Setup has completed.";
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Db2TestcontainerConfiguration" /> class.
+    /// </summary>
+    public Db2TestcontainerConfiguration()
+        : this(Db2Image)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Db2TestcontainerConfiguration" /> class.
+    /// </summary>
+    /// <param name="image">The Docker image.</param>
+    public Db2TestcontainerConfiguration(string image)
+      : base(image, Db2Port)
+    {
+      this.OutputConsumer = Consume.RedirectStdoutAndStderrToStream(new MemoryStream(), new MemoryStream());
+      this.WaitStrategy = Wait.ForUnixContainer().UntilMessageIsLogged(this.OutputConsumer.Stdout, WaitUntilMessageIsLogged);
+      this.Environments["LICENSE"] = "accept";
+      this.Environments["PERSISTENT_HOME"] = "false";
+    }
+
+    /// <inheritdoc />
+    public override string Database
+    {
+      get => this.Environments["DBNAME"];
+      set => this.Environments["DBNAME"] = value;
+    }
+
+    /// <inheritdoc />
+    public override string Username
+    {
+      get => "db2inst1";
+      set => throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public override string Password
+    {
+      get => this.Environments["DB2INST1_PASSWORD"];
+      set => this.Environments["DB2INST1_PASSWORD"] = value;
+    }
+
+    /// <inheritdoc />
+    public override IOutputConsumer OutputConsumer { get; }
+
+    /// <inheritdoc />
+    public override IWaitForContainerOS WaitStrategy { get; }
+  }
+}
---
+++ src/DotNet.Testcontainers/Containers/Modules/Databases/Db2Testcontainer.cs
@@ -0,0 +1,21 @@
+namespace DotNet.Testcontainers.Containers
+{
+  using DotNet.Testcontainers.Configurations;
+  using Microsoft.Extensions.Logging;
+
+  /// <inheritdoc cref="TestcontainerDatabase" />
+  public sealed class Db2Testcontainer : TestcontainerDatabase
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Db2Testcontainer" /> class.
+    /// </summary>
+    /// <param name="configuration">The Testcontainers configuration.</param>
+    /// <param name="logger">The logger.</param>
+    internal Db2Testcontainer(ITestcontainersConfiguration configuration, ILogger logger)
+      : base(configuration, logger)
+    {
+    }
+    /// <inheritdoc />
+    public override string ConnectionString => $"Server={Hostname}:{Port};Database={Database};User ID={Username};Password={Password};";
+  }
+}
