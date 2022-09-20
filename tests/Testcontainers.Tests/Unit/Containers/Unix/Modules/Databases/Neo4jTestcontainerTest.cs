9ab251cd2ace82a64d9a9cec046f6363400b2043
Donovan Bergin <kaiserbergin@gmail.com>
2022-09-19 21:10:07 -0500

    feat(#579): Add Neo4j module

---
+++ tests/Testcontainers.Tests/Unit/Containers/Unix/Modules/Databases/Neo4jTestcontainerTest.cs
@@ -0,0 +1,26 @@
+﻿namespace DotNet.Testcontainers.Tests.Unit
+{
+  using System.Threading.Tasks;
+  using DotNet.Testcontainers.Tests.Fixtures;
+  using Xunit;
+
+  [Collection(nameof(Testcontainers))]
+  public class Neo4jTestcontainerTest : IClassFixture<Neo4jFixture>
+  {
+    private readonly Neo4jFixture neo4jFixture;
+
+    public Neo4jTestcontainerTest(Neo4jFixture neo4jFixture)
+    {
+      this.neo4jFixture = neo4jFixture;
+    }
+
+    [Fact]
+    public async Task ConnectionEstablished()
+    {
+      var exception = await Record.ExceptionAsync(() => this.neo4jFixture.Connection.VerifyConnectivityAsync())
+        .ConfigureAwait(false);
+
+      Assert.Null(exception);
+    }
+  }
+}
