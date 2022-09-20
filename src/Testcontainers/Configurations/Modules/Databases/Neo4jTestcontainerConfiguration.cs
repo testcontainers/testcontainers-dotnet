74ebbdb8dab0aff08097d3b7ba29e814174e5d35
Donovan Bergin <kaiserbergin@gmail.com>
2022-09-19 21:10:07 -0500

    Adding support for neo4j

---
+++ src/Testcontainers/Configurations/Modules/Databases/Neo4jTestcontainerConfiguration.cs
@@ -0,0 +1,65 @@
+﻿namespace DotNet.Testcontainers.Configurations
+{
+  using System;
+  using DotNet.Testcontainers.Builders;
+  using JetBrains.Annotations;
+
+  /// <inheritdoc cref="TestcontainerDatabaseConfiguration" />
+  [PublicAPI]
+  public class Neo4jTestcontainerConfiguration : TestcontainerDatabaseConfiguration
+  {
+    private string password = DefaultPassword;
+
+    private const string Neo4jImage = "neo4j:latest";
+
+    private const int BoltPort = 7687;
+
+    private const string DefaultDatabase = "neo4j";
+
+    private const string DefaultUsername = "neo4j";
+
+    private const string DefaultPassword = "connect";
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Neo4jTestcontainerConfiguration" /> class.
+    /// </summary>
+    public Neo4jTestcontainerConfiguration()
+      : this(Neo4jImage)
+    {
+      this.Environments["NEO4JLABS_PLUGINS"] = "[\"apoc\"]";
+      this.Environments["NEO4J_ACCEPT_LICENSE_AGREEMENT"] = "yes";
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Neo4jTestcontainerConfiguration" /> class.
+    /// </summary>
+    /// <param name="image">The Docker image.</param>
+    public Neo4jTestcontainerConfiguration(string image)
+      : base(image, BoltPort)
+    {
+    }
+
+    /// <inheritdoc />
+    public override string Database => DefaultDatabase;
+
+    /// <inheritdoc />
+    public override string Username => DefaultUsername;
+
+    /// <inheritdoc />
+    public override string Password
+    {
+      get => this.password;
+
+      set
+      {
+        this.password = value ?? DefaultPassword;
+
+        this.Environments["NEO4J_AUTH"] = $"{this.Username}/{this.password}";
+      }
+    }
+
+    /// <inheritdoc />
+    public override IWaitForContainerOS WaitStrategy => Wait.ForUnixContainer()
+      .UntilPortIsAvailable(this.DefaultPort);
+  }
+}
---
+++ tests/Testcontainers.Tests/Unit/Containers/Unix/Modules/Databases/Neo4jTestcontainerTest.cs
@@ -0,0 +1,29 @@
+﻿namespace DotNet.Testcontainers.Tests.Unit
+{
+  using System.Threading.Tasks;
+  using DotNet.Testcontainers.Tests.Fixtures;
+  using Xunit;
+
+  [Collection(nameof(Testcontainers))]
+  public class Neo4jTestcontainerTest : IClassFixture<Neo4jFixture>
+  {
+    private readonly Neo4jFixture neo4jFixture;
+
+    public Neo4jTestcontainerTest(Neo4jFixture neo4jFixture)
+    {
+      this.neo4jFixture = neo4jFixture;
+    }
+
+    [Fact]
+    public async Task ConnectionEstablished()
+    {
+      // Given
+      var connection = this.neo4jFixture.Connection;
+
+      // When
+      await connection.VerifyConnectivityAsync();
+
+      // Then you don't throw an exception.
+    }
+  }
+}
