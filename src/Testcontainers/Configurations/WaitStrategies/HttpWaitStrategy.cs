26c39cd1d07ab905fc4c4cdc3909123dfd23f35d
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2022-12-20 13:47:59 +0100

    chore: Add XML docs

--- src/Testcontainers/Configurations/WaitStrategies/HttpWaitStrategy.cs
+++ src/Testcontainers/Configurations/WaitStrategies/HttpWaitStrategy.cs
@@ -10,9 +10,16 @@
   using JetBrains.Annotations;
   using Microsoft.Extensions.Logging;
 
+  /// <summary>
+  /// Wait for an HTTP(S) endpoint to return a particular status code.
+  /// </summary>
   [PublicAPI]
   public sealed class HttpWaitStrategy : IWaitUntil
   {
+    private const ushort HttpPort = 80;
+
+    private const ushort HttpsPort = 443;
+
     private readonly IDictionary<string, string> httpHeaders = new Dictionary<string, string>();
 
     private readonly ISet<HttpStatusCode> httpStatusCodes = new HashSet<HttpStatusCode>();
@@ -27,11 +34,15 @@
 
     private ushort portNumber;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HttpWaitStrategy" /> class.
+    /// </summary>
     public HttpWaitStrategy()
     {
-      _ = this.WithMethod(HttpMethod.Get).UsingTls(false).ForPort(80).ForPath("/");
+      _ = this.WithMethod(HttpMethod.Get).UsingTls(false).ForPort(HttpPort).ForPath("/");
     }
 
+    /// <inheritdoc />
     public async Task<bool> Until(ITestcontainersContainer testcontainers, ILogger logger)
     {
       string host;
@@ -93,48 +104,98 @@
       }
     }
 
+    /// <summary>
+    /// Waits for the status code.
+    /// </summary>
+    /// <param name="statusCode">The expected status code.</param>
+    /// <returns>A configured instance of <see cref="HttpWaitStrategy" />.</returns>
     public HttpWaitStrategy ForStatusCode(HttpStatusCode statusCode)
     {
       this.httpStatusCodes.Add(statusCode);
       return this;
     }
 
+    /// <summary>
+    /// Waits for the status code to pass the predicate.
+    /// </summary>
+    /// <param name="statusCodePredicate">The predicate to test the HTTP response against.</param>
+    /// <returns>A configured instance of <see cref="HttpWaitStrategy" />.</returns>
     public HttpWaitStrategy ForStatusCodeMatching(Predicate<HttpStatusCode> statusCodePredicate)
     {
       this.httpStatusCodePredicate = statusCodePredicate;
       return this;
     }
 
+    /// <summary>
+    /// Waits for the path.
+    /// </summary>
+    /// <param name="path">The path to check.</param>
+    /// <returns>A configured instance of <see cref="HttpWaitStrategy" />.</returns>
     public HttpWaitStrategy ForPath(string path)
     {
       this.pathValue = path;
       return this;
     }
 
+    /// <summary>
+    /// Waits for the port.
+    /// </summary>
+    /// <remarks>
+    /// <see cref="HttpPort" /> default value.
+    /// </remarks>
+    /// <param name="port">The port to check.</param>
+    /// <returns>A configured instance of <see cref="HttpWaitStrategy" />.</returns>
     public HttpWaitStrategy ForPort(ushort port)
     {
       this.portNumber = port;
       return this;
     }
 
+    /// <summary>
+    /// Indicates that the HTTP request use HTTPS.
+    /// </summary>
+    /// <remarks>
+    /// <see cref="bool.FalseString" /> default value.
+    /// </remarks>
+    /// <param name="tlsEnabled">True if the HTTP request use HTTPS, otherwise false.</param>
+    /// <returns>A configured instance of <see cref="HttpWaitStrategy" />.</returns>
     public HttpWaitStrategy UsingTls(bool tlsEnabled = true)
     {
       this.schemeName = tlsEnabled ? Uri.UriSchemeHttps : Uri.UriSchemeHttp;
       return this;
     }
 
+    /// <summary>
+    /// Indicates the HTTP request method.
+    /// </summary>
+    /// <remarks>
+    /// <see cref="HttpMethod.Get" /> default value.
+    /// </remarks>
+    /// <param name="method">The HTTP method.</param>
+    /// <returns>A configured instance of <see cref="HttpWaitStrategy" />.</returns>
     public HttpWaitStrategy WithMethod(HttpMethod method)
     {
       this.httpMethod = method;
       return this;
     }
 
+    /// <summary>
+    /// Adds a custom HTTP header to the HTTP request.
+    /// </summary>
+    /// <param name="name">The HTTP header name.</param>
+    /// <param name="value">The HTTP header value.</param>
+    /// <returns>A configured instance of <see cref="HttpWaitStrategy" />.</returns>
     public HttpWaitStrategy WithHeader(string name, string value)
     {
       this.httpHeaders.Add(name, value);
       return this;
     }
 
+    /// <summary>
+    /// Adds custom HTTP headers to the HTTP request.
+    /// </summary>
+    /// <param name="headers">A list of HTTP headers.</param>
+    /// <returns>A configured instance of <see cref="HttpWaitStrategy" />.</returns>
     public HttpWaitStrategy WithHeaders(IReadOnlyDictionary<string, string> headers)
     {
       foreach (var header in headers)
