c5c79d69b95c127d1da7c85c18c381bbf14cbc9c
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2023-02-03 11:34:46 +0100

    chore: Add Microsoft SQL Server builde test project

---
+++ tests/Testcontainers.MsSql.Tests/MsSqlContainerTest.cs
@@ -0,0 +1,29 @@
+﻿namespace Testcontainers.MsSql.Tests;
+
+public sealed class MsSqlContainerTest : IAsyncLifetime
+{
+    private readonly MsSqlContainer _msSqlContainer = new MsSqlBuilder().Build();
+
+    public Task InitializeAsync()
+    {
+        return _msSqlContainer.StartAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return _msSqlContainer.DisposeAsync().AsTask();
+    }
+
+    [Fact]
+    public void ConnectionStateReturnsOpen()
+    {
+        // Given
+        using DbConnection connection = new SqlConnection(_msSqlContainer.GetConnectionString());
+
+        // When
+        connection.Open();
+
+        // Then
+        Assert.Equal(ConnectionState.Open, connection.State);
+    }
+}
