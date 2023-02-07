6c6436571ad4fabe3239f459b565ad70eaa76117
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2023-02-07 17:32:52 +0100

    fix: Replace MySql.Data with MySqlConnector due to build warnings of the dependency

--- tests/Testcontainers.MySql.Tests/Usings.cs
+++ tests/Testcontainers.MySql.Tests/Usings.cs
@@ -3,5 +3,5 @@ global using System.Data.Common;
 global using System.Threading.Tasks;
 global using DotNet.Testcontainers.Commons;
 global using JetBrains.Annotations;
-global using MySql.Data.MySqlClient;
+global using MySqlConnector;
 global using Xunit;
