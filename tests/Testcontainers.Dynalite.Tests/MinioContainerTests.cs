6b1583837937593d3403e0f129402e1df415f078
Dominik.Kotecki <dominikus1910@gmail.com>
2023-02-08 18:54:35 +0100

    add tests

--- tests/Testcontainers.Dynalite.Tests/MinioContainerTests.cs
+++ tests/Testcontainers.Dynalite.Tests/MinioContainerTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.Model;
 using Amazon.Runtime;
+using DotNet.Testcontainers;
 
 public sealed class MinioContainerTest : IAsyncLifetime
 {
@@ -35,7 +36,7 @@ public sealed class MinioContainerTest : IAsyncLifetime
             TableName = "TestDynamoDbTable",
             AttributeDefinitions = new List<AttributeDefinition>()
             {
-              new AttributeDefinition("Id", ScalarAttributeType.N),
+              new AttributeDefinition("Id", ScalarAttributeType.S),
               new AttributeDefinition("Name", ScalarAttributeType.S),
             },
             KeySchema = new List<KeySchemaElement>()
@@ -52,34 +53,44 @@ public sealed class MinioContainerTest : IAsyncLifetime
         Assert.Equal(HttpStatusCode.OK, tableResponse.HttpStatusCode);
     }
 
-    // [Fact]
-    // [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
-    // public async Task GetObjectReturnsPutObject()
-    // {
-    //     // Given
-    //     using var inputStream = new MemoryStream(new byte[byte.MaxValue]);
-    //
-    //     var config = new AmazonS3Config();
-    //     config.ServiceURL = this._dynaliteContainer.GetEndpoint();
-    //
-    //     var client = new AmazonS3Client(this._dynaliteContainer.GetAccessKeyId(), this._dynaliteContainer.GetAccessSecret(), config);
-    //
-    //     var objectRequest = new PutObjectRequest();
-    //     objectRequest.BucketName = Guid.NewGuid().ToString("D");
-    //     objectRequest.Key = Guid.NewGuid().ToString("D");
-    //     objectRequest.InputStream = inputStream;
-    //
-    //     // When
-    //     _ = await client.PutBucketAsync(objectRequest.BucketName)
-    //         .ConfigureAwait(false);
-    //
-    //     _ = await client.PutObjectAsync(objectRequest)
-    //         .ConfigureAwait(false);
-    //
-    //     var objectResponse = await client.GetObjectAsync(objectRequest.BucketName, objectRequest.Key)
-    //         .ConfigureAwait(false);
-    //
-    //     // Then
-    //     Assert.Equal(byte.MaxValue, objectResponse.ContentLength);
-    // }
+    [Fact]
+    [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+    public async Task InsertElementToTableReturnsInsertedElement()
+    {
+      // Given
+      var tableName = $"TestDynamoDbTable-{Guid.NewGuid():D}";
+      var itemId = Guid.NewGuid().ToString("D");
+
+      var clientConfig = new AmazonDynamoDBConfig();
+      clientConfig.ServiceURL = this._dynaliteContainer.GetEndpoint();
+      clientConfig.UseHttp = true;
+      using var client = new AmazonDynamoDBClient(new BasicAWSCredentials("dummy", "dummy"), clientConfig);
+
+      // When
+      _ = await client.CreateTableAsync(new CreateTableRequest()
+        {
+          TableName = tableName,
+          AttributeDefinitions = new List<AttributeDefinition>()
+          {
+            new AttributeDefinition("Id", ScalarAttributeType.S),
+            new AttributeDefinition("Name", ScalarAttributeType.S),
+          },
+          KeySchema = new List<KeySchemaElement>()
+          {
+            new KeySchemaElement("Id", KeyType.HASH),
+            new KeySchemaElement("Name", KeyType.RANGE),
+          },
+          ProvisionedThroughput = new ProvisionedThroughput(1, 1),
+          TableClass = TableClass.STANDARD,
+        })
+        .ConfigureAwait(false);
+
+       var response = await client.PutItemAsync(new PutItemRequest(tableName, new Dictionary<string, AttributeValue>() { { "Id", new AttributeValue() { S = itemId } }, { "Name", new AttributeValue() { S = "Test" } } }));
+       Assert.Equal(HttpStatusCode.OK, response.HttpStatusCode);
+
+        var getItemResponse = await client.GetItemAsync(new GetItemRequest(tableName, new Dictionary<string, AttributeValue>() { { "Id", new AttributeValue() { S = itemId } }, { "Name", new AttributeValue() { S = "Test" } } }));
+
+        // Then
+        Assert.Equal(HttpStatusCode.OK, getItemResponse.HttpStatusCode);
+    }
 }
