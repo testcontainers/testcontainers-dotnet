9d8a11723f25f9b88d2b8711baa8b77171c2b52b
Dominik.Kotecki <dominikus1910@gmail.com>
2023-02-08 18:13:02 +0100

    add usings files and editorconfig

--- src/Testcontainers.Dynalite/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Testcontainers.Dynalite;
-
-public class Class1
-{
-}
---
+++ tests/Testcontainers.Dynalite.Tests/Usings.cs
@@ -0,0 +1,6 @@
+global using System;
+global using System.IO;
+global using System.Threading.Tasks;
+global using Xunit;
+global using System.Net;
+global using DotNet.Testcontainers.Commons;
