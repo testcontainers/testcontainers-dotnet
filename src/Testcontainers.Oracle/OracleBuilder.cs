032e74db7848a42d9e2c65e78f72aa0be6853177
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2023-02-09 15:23:49 +0100

    feat: Replace Oracle module (#770)

---
+++ src/Testcontainers.Oracle/OracleBuilder.cs
@@ -0,0 +1,131 @@
+namespace Testcontainers.Oracle;
+
+/// <inheritdoc cref="ContainerBuilder{TBuilderEntity, TContainerEntity, TConfigurationEntity}" />
+[PublicAPI]
+public sealed class OracleBuilder : ContainerBuilder<OracleBuilder, OracleContainer, OracleConfiguration>
+{
+    public const string OracleImage = "gvenzl/oracle-xe:21.3.0-slim-faststart";
+
+    public const ushort OraclePort = 1521;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OracleBuilder" /> class.
+    /// </summary>
+    public OracleBuilder()
+        : this(new OracleConfiguration())
+    {
+        DockerResourceConfiguration = Init().DockerResourceConfiguration;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OracleBuilder" /> class.
+    /// </summary>
+    /// <param name="resourceConfiguration">The Docker resource configuration.</param>
+    private OracleBuilder(OracleConfiguration resourceConfiguration)
+        : base(resourceConfiguration)
+    {
+        DockerResourceConfiguration = resourceConfiguration;
+    }
+
+    /// <inheritdoc />
+    protected override OracleConfiguration DockerResourceConfiguration { get; }
+
+    /// <summary>
+    /// Sets the Oracle password.
+    /// </summary>
+    /// <param name="password">The Oracle password.</param>
+    /// <returns>A configured instance of <see cref="OracleBuilder" />.</returns>
+    public OracleBuilder WithPassword(string password)
+    {
+        return Merge(DockerResourceConfiguration, new OracleConfiguration(password: password))
+            .WithEnvironment("ORACLE_PASSWORD", password)
+            .WithEnvironment("APP_USER_PASSWORD", password);
+    }
+
+    /// <inheritdoc />
+    public override OracleContainer Build()
+    {
+        Validate();
+        return new OracleContainer(DockerResourceConfiguration, TestcontainersSettings.Logger);
+    }
+
+    /// <inheritdoc />
+    protected override OracleBuilder Init()
+    {
+        return base.Init()
+            .WithImage(OracleImage)
+            .WithPortBinding(OraclePort, true)
+            .WithDatabase("XEPDB1")
+            .WithUsername("oracle")
+            .WithPassword(Guid.NewGuid().ToString("N").Substring(0, 16))
+            .WithWaitStrategy(Wait.ForUnixContainer().AddCustomWaitStrategy(new WaitUntil()));
+    }
+
+    /// <inheritdoc />
+    protected override void Validate()
+    {
+        base.Validate();
+
+        _ = Guard.Argument(DockerResourceConfiguration.Password, nameof(DockerResourceConfiguration.Password))
+            .NotNull()
+            .NotEmpty();
+    }
+
+    /// <inheritdoc />
+    protected override OracleBuilder Clone(IResourceConfiguration<CreateContainerParameters> resourceConfiguration)
+    {
+        return Merge(DockerResourceConfiguration, new OracleConfiguration(resourceConfiguration));
+    }
+
+    /// <inheritdoc />
+    protected override OracleBuilder Clone(IContainerConfiguration resourceConfiguration)
+    {
+        return Merge(DockerResourceConfiguration, new OracleConfiguration(resourceConfiguration));
+    }
+
+    /// <inheritdoc />
+    protected override OracleBuilder Merge(OracleConfiguration oldValue, OracleConfiguration newValue)
+    {
+        return new OracleBuilder(new OracleConfiguration(oldValue, newValue));
+    }
+
+    /// <summary>
+    /// Sets the Oracle database.
+    /// </summary>
+    /// <remarks>
+    /// The Docker image does not allow to configure the database.
+    /// </remarks>
+    /// <param name="database">The Oracle database.</param>
+    /// <returns>A configured instance of <see cref="OracleBuilder" />.</returns>
+    private OracleBuilder WithDatabase(string database)
+    {
+        return Merge(DockerResourceConfiguration, new OracleConfiguration(database: database));
+    }
+
+    /// <summary>
+    /// Sets the Oracle username.
+    /// </summary>
+    /// <remarks>
+    /// The Docker image does not allow to configure the username.
+    /// </remarks>
+    /// <param name="username">The Oracle username.</param>
+    /// <returns>A configured instance of <see cref="OracleBuilder" />.</returns>
+    private OracleBuilder WithUsername(string username)
+    {
+        return Merge(DockerResourceConfiguration, new OracleConfiguration(username: username))
+            .WithEnvironment("APP_USER", username);
+    }
+
+    /// <inheritdoc cref="IWaitUntil" />
+    private sealed class WaitUntil : IWaitUntil
+    {
+        /// <inheritdoc />
+        public async Task<bool> UntilAsync(IContainer container)
+        {
+            var (stdout, _) = await container.GetLogs()
+                .ConfigureAwait(false);
+
+            return stdout.Contains("DATABASE IS READY TO USE!");
+        }
+    }
+}
---
+++ tests/Testcontainers.Oracle.Tests/OracleContainerTest.cs
@@ -0,0 +1,45 @@
+﻿namespace Testcontainers.Oracle;
+
+public sealed class OracleContainerTest : IAsyncLifetime
+{
+    private readonly OracleContainer _oracleContainer = new OracleBuilder().Build();
+
+    public Task InitializeAsync()
+    {
+        return _oracleContainer.StartAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return _oracleContainer.DisposeAsync().AsTask();
+    }
+
+    [Fact]
+    [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+    public void ConnectionStateReturnsOpen()
+    {
+        // Given
+        using DbConnection connection = new OracleConnection(_oracleContainer.GetConnectionString());
+
+        // When
+        connection.Open();
+
+        // Then
+        Assert.Equal(ConnectionState.Open, connection.State);
+    }
+
+    [Fact]
+    [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+    public async Task ExecScriptReturnsSuccessful()
+    {
+        // Given
+        const string scriptContent = "SELECT 1 FROM DUAL;";
+
+        // When
+        var execResult = await _oracleContainer.ExecScriptAsync(scriptContent)
+            .ConfigureAwait(false);
+
+        // When
+        Assert.True(0L.Equals(execResult.ExitCode), execResult.Stderr);
+    }
+}
