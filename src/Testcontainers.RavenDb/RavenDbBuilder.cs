837aa540057cdb5992b55ab8a20c45a796011337
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2023-02-09 10:52:26 +0100

    feat: Add RavenDB module

---
+++ src/Testcontainers.RavenDb/RavenDbBuilder.cs
@@ -0,0 +1,79 @@
+namespace Testcontainers.RavenDb;
+
+/// <inheritdoc cref="ContainerBuilder{TBuilderEntity, TContainerEntity, TConfigurationEntity}" />
+[PublicAPI]
+public sealed class RavenDbBuilder : ContainerBuilder<RavenDbBuilder, RavenDbContainer, RavenDbConfiguration>
+{
+    public const string RavenDbImage = "ravendb/ravendb:5.4-ubuntu-latest";
+
+    public const ushort RavenDbPort = 8080;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RavenDbBuilder" /> class.
+    /// </summary>
+    public RavenDbBuilder()
+        : this(new RavenDbConfiguration())
+    {
+        DockerResourceConfiguration = Init().DockerResourceConfiguration;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RavenDbBuilder" /> class.
+    /// </summary>
+    /// <param name="resourceConfiguration">The Docker resource configuration.</param>
+    private RavenDbBuilder(RavenDbConfiguration resourceConfiguration)
+        : base(resourceConfiguration)
+    {
+        DockerResourceConfiguration = resourceConfiguration;
+    }
+
+    /// <inheritdoc />
+    protected override RavenDbConfiguration DockerResourceConfiguration { get; }
+
+    /// <inheritdoc />
+    public override RavenDbContainer Build()
+    {
+        Validate();
+        return new RavenDbContainer(DockerResourceConfiguration, TestcontainersSettings.Logger);
+    }
+
+    /// <inheritdoc />
+    protected override RavenDbBuilder Init()
+    {
+        return base.Init()
+            .WithImage(RavenDbImage)
+            .WithPortBinding(RavenDbPort, true)
+            .WithWaitStrategy(Wait.ForUnixContainer().AddCustomWaitStrategy(new WaitUntil()));
+    }
+
+    /// <inheritdoc />
+    protected override RavenDbBuilder Clone(IResourceConfiguration<CreateContainerParameters> resourceConfiguration)
+    {
+        return Merge(DockerResourceConfiguration, new RavenDbConfiguration(resourceConfiguration));
+    }
+
+    /// <inheritdoc />
+    protected override RavenDbBuilder Clone(IContainerConfiguration resourceConfiguration)
+    {
+        return Merge(DockerResourceConfiguration, new RavenDbConfiguration(resourceConfiguration));
+    }
+
+    /// <inheritdoc />
+    protected override RavenDbBuilder Merge(RavenDbConfiguration oldValue, RavenDbConfiguration newValue)
+    {
+        return new RavenDbBuilder(new RavenDbConfiguration(oldValue, newValue));
+    }
+
+    /// <inheritdoc cref="IWaitUntil" />
+    private sealed class WaitUntil : IWaitUntil
+    {
+        /// <inheritdoc />
+        public async Task<bool> UntilAsync(IContainer container)
+        {
+            var (stdout, _) = await container.GetLogs()
+                .ConfigureAwait(false);
+
+            return stdout.Contains("Server started");
+        }
+    }
+}
---
+++ tests/Testcontainers.RavenDb.Tests/RavenDbContainerTest.cs
@@ -0,0 +1,32 @@
+﻿namespace Testcontainers.RavenDb;
+
+public sealed class RavenDbContainerTest : IAsyncLifetime
+{
+    private readonly RavenDbContainer _ravenDbContainer = new RavenDbBuilder().Build();
+
+    public Task InitializeAsync()
+    {
+        return _ravenDbContainer.StartAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return _ravenDbContainer.DisposeAsync().AsTask();
+    }
+
+    [Fact]
+    [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+    public void GetBuildNumberOperationReturnsBuildNumber()
+    {
+        // Given
+        using var documentStore = new DocumentStore();
+        documentStore.Urls = new[] { _ravenDbContainer.GetEndpoint() };
+        documentStore.Initialize();
+
+        // When
+        var buildNumber = documentStore.Maintenance.Server.Send(new GetBuildNumberOperation());
+
+        // Then
+        Assert.Equal("5.4", buildNumber.ProductVersion);
+    }
+}
