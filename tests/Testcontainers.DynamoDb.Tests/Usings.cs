ffaa1568f9df5822b43c0f43a48cfc7ad4dec989
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2023-02-10 12:38:26 +0100

    chore: Clean up DynamoDB module

--- tests/Testcontainers.DynamoDb.Tests/Usings.cs
+++ tests/Testcontainers.DynamoDb.Tests/Usings.cs
@@ -1,6 +1,5 @@
 global using System;
 global using System.Collections.Generic;
-global using System.IO;
 global using System.Net;
 global using System.Threading.Tasks;
 global using Amazon.DynamoDBv2;
