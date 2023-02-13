b0b52b691a8eb31b00bac96fdbfe96ad25e193bd
Dominik Kotecki <dominikus1993@users.noreply.github.com>
2023-02-13 19:22:05 +0100

    feat: Add LocalStack module (#773)
    Co-authored-by: Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>

---
+++ src/Testcontainers.LocalStack/LocalStackContainer.cs
@@ -0,0 +1,28 @@
+namespace Testcontainers.LocalStack;
+
+/// <inheritdoc cref="DockerContainer" />
+[PublicAPI]
+public sealed class LocalStackContainer : DockerContainer
+{
+    private readonly LocalStackConfiguration _configuration;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LocalStackContainer" /> class.
+    /// </summary>
+    /// <param name="configuration">The container configuration.</param>
+    /// <param name="logger">The logger.</param>
+    public LocalStackContainer(LocalStackConfiguration configuration, ILogger logger)
+        : base(configuration, logger)
+    {
+        _configuration = configuration;
+    }
+
+    /// <summary>
+    /// Gets the LocalStack endpoint.
+    /// </summary>
+    /// <returns>The LocalStack endpoint.</returns>
+    public string GetEndpoint()
+    {
+        return new UriBuilder(Uri.UriSchemeHttp, Hostname, GetMappedPublicPort(LocalStackBuilder.LocalStackPort)).ToString();
+    }
+}
---
+++ tests/Testcontainers.LocalStack.Tests/LocalStackContainerTests.cs
@@ -0,0 +1,132 @@
+﻿namespace Testcontainers.LocalStack;
+
+public sealed class LocalStackContainerTest : IAsyncLifetime
+{
+    private readonly LocalStackContainer _localStackContainer = new LocalStackBuilder().Build();
+
+    static LocalStackContainerTest()
+    {
+        Environment.SetEnvironmentVariable("AWS_ACCESS_KEY_ID", "AKIAIOSFODNN7EXAMPLE"); 
+        Environment.SetEnvironmentVariable("AWS_SECRET_ACCESS_KEY", "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"); 
+    }
+    
+    public Task InitializeAsync()
+    {
+        return _localStackContainer.StartAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return _localStackContainer.DisposeAsync().AsTask();
+    }
+
+    [Fact]
+    [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+    public async Task ListAmazonS3BucketsReturnsHttpStatusCodeOk()
+    {
+        // Given
+        var config = new AmazonS3Config();
+        config.ServiceURL = _localStackContainer.GetEndpoint();
+
+        var client = new AmazonS3Client(config);
+
+        // When
+        var buckets = await client.ListBucketsAsync()
+            .ConfigureAwait(false);
+
+        // Then
+        Assert.Equal(HttpStatusCode.OK, buckets.HttpStatusCode);
+    }
+
+    [Fact]
+    [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+    public async Task CreateDynamoDbTableReturnsCorrectTableDescription()
+    {
+        // Given
+        const string tableName = "TestDynamoDbTable";
+        var clientConfig = new AmazonDynamoDBConfig();
+        clientConfig.ServiceURL = this._localStackContainer.GetEndpoint();
+        clientConfig.UseHttp = true;
+        using var client = new AmazonDynamoDBClient(clientConfig);
+
+        // When
+        _ = await client.CreateTableAsync(new CreateTableRequest()
+            {
+                TableName = tableName,
+                AttributeDefinitions = new List<AttributeDefinition>()
+                {
+                    new AttributeDefinition("Id", ScalarAttributeType.S),
+                    new AttributeDefinition("Name", ScalarAttributeType.S),
+                },
+                KeySchema = new List<KeySchemaElement>()
+                    { new KeySchemaElement("Id", KeyType.HASH), new KeySchemaElement("Name", KeyType.RANGE), },
+                ProvisionedThroughput = new ProvisionedThroughput(1, 1),
+                TableClass = TableClass.STANDARD,
+            })
+            .ConfigureAwait(false);
+
+        var tableDescription = await client.DescribeTableAsync(tableName).ConfigureAwait(false);
+
+        // Then
+        Assert.NotNull(tableDescription);
+        Assert.Equal(HttpStatusCode.OK, tableDescription.HttpStatusCode);
+        Assert.Equal(tableName, tableDescription.Table.TableName);
+        Assert.Equal("Id", tableDescription.Table.KeySchema[0].AttributeName);
+    }
+
+    [Fact]
+    [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+    public async Task CreateSqsQueueReturnsHttpStatusCodeOk()
+    {
+        // Given
+        string queueName = Guid.NewGuid().ToString("D");
+        var clientConfig = new AmazonSQSConfig();
+        clientConfig.ServiceURL = this._localStackContainer.GetEndpoint();
+        clientConfig.UseHttp = true;
+        using var client = new AmazonSQSClient(clientConfig);
+
+        // When
+        var response = await client.CreateQueueAsync(new CreateQueueRequest(queueName));
+
+        // Then
+        Assert.Equal(HttpStatusCode.OK, response.HttpStatusCode);
+    }
+
+    [Fact]
+    [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+    public async Task CreateLogGroupReturnsHttpStatusCodeOk()
+    {
+        // Given
+        string logGroupName = Guid.NewGuid().ToString("D");
+        var clientConfig = new AmazonCloudWatchLogsConfig();
+        clientConfig.ServiceURL = this._localStackContainer.GetEndpoint();
+        clientConfig.UseHttp = true;
+        using var client = new AmazonCloudWatchLogsClient(clientConfig);
+
+        // When
+        var response = await client.CreateLogGroupAsync(new CreateLogGroupRequest(logGroupName));
+
+        // Then
+        Assert.Equal(HttpStatusCode.OK, response.HttpStatusCode);
+    }
+
+
+    [Fact]
+    [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+    public async Task CreateSnsTopicReturnsHttpStatusCodeOk()
+    {
+        // Given
+        string topicName = Guid.NewGuid().ToString("D");
+        var clientConfig = new AmazonSimpleNotificationServiceConfig();
+        clientConfig.ServiceURL = this._localStackContainer.GetEndpoint();
+        clientConfig.UseHttp = true;
+        using var client =
+            new AmazonSimpleNotificationServiceClient(clientConfig);
+
+        // When
+        var response = await client.CreateTopicAsync(new CreateTopicRequest(topicName));
+
+        // Then
+        Assert.Equal(HttpStatusCode.OK, response.HttpStatusCode);
+    }
+}
