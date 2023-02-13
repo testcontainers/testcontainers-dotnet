f25c3dcd2b38e976d3180282a8eb2fc3edcbab9b
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2023-02-13 15:28:46 +0100

    feat: Replace CouchDb module (#779)

---
+++ tests/Testcontainers.CouchDb.Tests/CouchDbContainerTest.cs
@@ -0,0 +1,30 @@
+﻿namespace Testcontainers.CouchDb;
+
+public sealed class CouchDbContainerTest : IAsyncLifetime
+{
+    private readonly CouchDbContainer _couchDbContainer = new CouchDbBuilder().Build();
+
+    public Task InitializeAsync()
+    {
+        return _couchDbContainer.StartAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return _couchDbContainer.DisposeAsync().AsTask();
+    }
+
+    [Fact]
+    public async Task PutDatabaseReturnsHttpStatusCodeCreated()
+    {
+        // Given
+        using var client = new MyCouchClient(_couchDbContainer.GetConnectionString(), "db");
+
+        // When
+        var database = await client.Database.PutAsync()
+            .ConfigureAwait(false);
+
+        // Then
+        Assert.Equal(HttpStatusCode.Created, database.StatusCode);
+    }
+}
---
+++ tests/Testcontainers.CouchDb.Tests/Usings.cs
@@ -0,0 +1,4 @@
+global using System.Net;
+global using System.Threading.Tasks;
+global using MyCouch;
+global using Xunit;
