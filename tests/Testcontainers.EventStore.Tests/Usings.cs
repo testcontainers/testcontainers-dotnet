6a9b37e5a1dd8dd87f859a1cd73381799d791edd
Meysam Hadeli <35596795+meysamhadeli@users.noreply.github.com>
2023-02-17 13:33:32 +0330

    Update tests/Testcontainers.EventStore.Tests/Usings.cs
    Co-authored-by: Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>

--- tests/Testcontainers.EventStore.Tests/Usings.cs
+++ tests/Testcontainers.EventStore.Tests/Usings.cs
@@ -1,2 +1,7 @@
-﻿global using System.Threading.Tasks;
+global using System;
+global using System.Linq;
+global using System.Text.Json;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using EventStore.Client;
 global using Xunit;
