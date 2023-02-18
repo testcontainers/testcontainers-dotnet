955d70949f766e9459fd0657d1a677a1b11d5ece
Meysam Hadeli <35596795+meysamhadeli@users.noreply.github.com>
2023-02-18 12:50:27 +0330

    feat: Add EventStoreDb module (#778)
    Co-authored-by: Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>

---
+++ src/Testcontainers.EventStoreDb/EventStoreDbContainer.cs
@@ -0,0 +1,27 @@
+﻿namespace Testcontainers.EventStore;
+
+/// <inheritdoc cref="DockerContainer" />
+[PublicAPI]
+public sealed class EventStoreDbContainer : DockerContainer
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EventStoreDbContainer" /> class.
+    /// </summary>
+    /// <param name="configuration">The container configuration.</param>
+    /// <param name="logger">The logger.</param>
+    public EventStoreDbContainer(EventStoreDbConfiguration configuration, ILogger logger)
+        : base(configuration, logger)
+    {
+    }
+
+    /// <summary>
+    /// Gets the EventStore connection string.
+    /// </summary>
+    /// <returns>The EventStore connection string.</returns>
+    public string GetConnectionString()
+    {
+        var endpoint = new UriBuilder("esdb", Hostname, GetMappedPublicPort(EventStoreDbBuilder.EventStorePort));
+        endpoint.Query = "tls=false";
+        return endpoint.ToString();
+    }
+}
---
+++ tests/Testcontainers.EventStoreDb.Tests/Usings.cs
@@ -0,0 +1,7 @@
+global using System;
+global using System.Linq;
+global using System.Text.Json;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using EventStore.Client;
+global using Xunit;
