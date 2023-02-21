06ade0868eeb551c8e38d2c3e0ec69f1c69b6cfd
Eddú Meléndez <eddu.melendez@gmail.com>
2023-02-21 12:39:24 -0600

    Add Redpanda module
    Redpanda provides a Kafka compatible API.

---
+++ src/Testcontainers.Redpanda/RedpandaConfiguration.cs
@@ -0,0 +1,58 @@
+namespace Testcontainers.Redpanda;
+
+/// <inheritdoc cref="ContainerConfiguration" />
+[PublicAPI]
+public sealed class RedpandaConfiguration : ContainerConfiguration
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RedpandaConfiguration" /> class.
+    /// </summary>
+    /// <param name="config">The Redpanda config.</param>
+    public RedpandaConfiguration()
+    {
+        // // Sets the custom builder methods property values.
+        // Config = config;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RedpandaConfiguration" /> class.
+    /// </summary>
+    /// <param name="resourceConfiguration">The Docker resource configuration.</param>
+    public RedpandaConfiguration(IResourceConfiguration<CreateContainerParameters> resourceConfiguration)
+        : base(resourceConfiguration)
+    {
+        // Passes the configuration upwards to the base implementations to create an updated immutable copy.
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RedpandaConfiguration" /> class.
+    /// </summary>
+    /// <param name="resourceConfiguration">The Docker resource configuration.</param>
+    public RedpandaConfiguration(IContainerConfiguration resourceConfiguration)
+        : base(resourceConfiguration)
+    {
+        // Passes the configuration upwards to the base implementations to create an updated immutable copy.
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RedpandaConfiguration" /> class.
+    /// </summary>
+    /// <param name="resourceConfiguration">The Docker resource configuration.</param>
+    public RedpandaConfiguration(RedpandaConfiguration resourceConfiguration)
+        : this(new RedpandaConfiguration(), resourceConfiguration)
+    {
+        // Passes the configuration upwards to the base implementations to create an updated immutable copy.
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RedpandaConfiguration" /> class.
+    /// </summary>
+    /// <param name="oldValue">The old Docker resource configuration.</param>
+    /// <param name="newValue">The new Docker resource configuration.</param>
+    public RedpandaConfiguration(RedpandaConfiguration oldValue, RedpandaConfiguration newValue)
+        : base(oldValue, newValue)
+    {
+        // // Create an updated immutable copy of the module configuration.
+        // Config = BuildConfiguration.Combine(oldValue.Config, newValue.Config);
+    }
+}
---
+++ tests/Testcontainers.Redpanda.Tests/RedpandaContainerTest.cs
@@ -0,0 +1,66 @@
+﻿namespace Testcontainers.Redpanda;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+public sealed class RedpandaContainerTest : IAsyncLifetime
+{
+  private readonly RedpandaContainer _redpandaContainer = new RedpandaBuilder().Build();
+
+  public Task InitializeAsync()
+  {
+    return this._redpandaContainer.StartAsync();
+  }
+
+  public Task DisposeAsync()
+  {
+    return this._redpandaContainer.DisposeAsync().AsTask();
+  }
+
+  [Fact]
+  [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+  public async Task consumeMessage()
+  {
+    const string topic = "sample";
+
+    var producerConfig = new ReadOnlyDictionary<string, string>(
+      new Dictionary<string, string>
+      {
+        { "bootstrap.servers", this._redpandaContainer.GetBootstrapServers() },
+      });
+
+    var consumerConfig = new ReadOnlyDictionary<string, string>(
+      new Dictionary<string, string>
+      {
+        { "bootstrap.servers", this._redpandaContainer.GetBootstrapServers() },
+        { "auto.offset.reset", "earliest" },
+        { "group.id", "sample-consumer" },
+      });
+
+    var message = new Message<string, string>
+    {
+      Value = Guid.NewGuid().ToString(),
+    };
+
+    var deliveryReportTaskCompletionSource = new TaskCompletionSource<DeliveryReport<string, string>>();
+
+    ConsumeResult<string, string> result;
+
+    using (var producer = new ProducerBuilder<string, string>(producerConfig).Build())
+    {
+      producer.Produce(topic, message, deliveryHandler => deliveryReportTaskCompletionSource.SetResult(deliveryHandler));
+      await deliveryReportTaskCompletionSource.Task;
+    }
+
+    using (var consumer = new ConsumerBuilder<string, string>(consumerConfig).Build())
+    {
+      consumer.Subscribe(topic);
+      result = consumer.Consume(TimeSpan.FromSeconds(5));
+    }
+
+    Assert.NotNull(result);
+    Assert.Equal(message.Value, result.Message.Value);
+  }
+
+}
