56e567ca141d2a621611b3d82ff0378b65246446
Eddú Meléndez Gonzales <eddu.melendez@gmail.com>
2023-02-22 09:51:51 -0600

    Update tests/Testcontainers.Redpanda.Tests/RedpandaContainerTest.cs
    Co-authored-by: Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>

--- tests/Testcontainers.Redpanda.Tests/RedpandaContainerTest.cs
+++ tests/Testcontainers.Redpanda.Tests/RedpandaContainerTest.cs
@@ -1,66 +1,56 @@
-﻿namespace Testcontainers.Redpanda;
-
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+namespace Testcontainers.Redpanda;
 
 public sealed class RedpandaContainerTest : IAsyncLifetime
 {
-  private readonly RedpandaContainer _redpandaContainer = new RedpandaBuilder().Build();
+    private readonly RedpandaContainer _redpandaContainer = new RedpandaBuilder().Build();
 
-  public Task InitializeAsync()
-  {
-    return this._redpandaContainer.StartAsync();
-  }
-
-  public Task DisposeAsync()
-  {
-    return this._redpandaContainer.DisposeAsync().AsTask();
-  }
-
-  [Fact]
-  [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
-  public async Task consumeMessage()
-  {
-    const string topic = "sample";
-
-    var producerConfig = new ReadOnlyDictionary<string, string>(
-      new Dictionary<string, string>
-      {
-        { "bootstrap.servers", this._redpandaContainer.GetBootstrapServers() },
-      });
-
-    var consumerConfig = new ReadOnlyDictionary<string, string>(
-      new Dictionary<string, string>
-      {
-        { "bootstrap.servers", this._redpandaContainer.GetBootstrapServers() },
-        { "auto.offset.reset", "earliest" },
-        { "group.id", "sample-consumer" },
-      });
-
-    var message = new Message<string, string>
+    public Task InitializeAsync()
     {
-      Value = Guid.NewGuid().ToString(),
-    };
-
-    var deliveryReportTaskCompletionSource = new TaskCompletionSource<DeliveryReport<string, string>>();
-
-    ConsumeResult<string, string> result;
-
-    using (var producer = new ProducerBuilder<string, string>(producerConfig).Build())
-    {
-      producer.Produce(topic, message, deliveryHandler => deliveryReportTaskCompletionSource.SetResult(deliveryHandler));
-      await deliveryReportTaskCompletionSource.Task;
+        return _redpandaContainer.StartAsync();
     }
 
-    using (var consumer = new ConsumerBuilder<string, string>(consumerConfig).Build())
+    public Task DisposeAsync()
     {
-      consumer.Subscribe(topic);
-      result = consumer.Consume(TimeSpan.FromSeconds(5));
+        return _redpandaContainer.DisposeAsync().AsTask();
     }
 
-    Assert.NotNull(result);
-    Assert.Equal(message.Value, result.Message.Value);
-  }
+    [Fact]
+    [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+    public async Task ConsumerReturnsProducerMessage()
+    {
+        // Given
+        const string topic = "sample";
 
+        var producerConfig = new ProducerConfig();
+        producerConfig.BootstrapServers = _redpandaContainer.GetBootstrapServers();
+
+        var consumerConfig = new ConsumerConfig();
+        consumerConfig.BootstrapServers = _redpandaContainer.GetBootstrapServers();
+        consumerConfig.GroupId = "sample-consumer";
+        consumerConfig.AutoOffsetReset = AutoOffsetReset.Earliest;
+
+        var message = new Message<string, string>
+        {
+            Value = Guid.NewGuid().ToString("D"),
+        };
+
+        // When
+        ConsumeResult<string, string> result;
+
+        using (var producer = new ProducerBuilder<string, string>(producerConfig).Build())
+        {
+            _ = await producer.ProduceAsync(topic, message)
+                .ConfigureAwait(false);
+        }
+
+        using (var consumer = new ConsumerBuilder<string, string>(consumerConfig).Build())
+        {
+            consumer.Subscribe(topic);
+            result = consumer.Consume(TimeSpan.FromSeconds(15));
+        }
+
+        // Then
+        Assert.NotNull(result);
+        Assert.Equal(message.Value, result.Message.Value);
+    }
 }
