e9152637f9600875f81e6f52577ec8d17064cb0e
Eddú Meléndez Gonzales <eddu.melendez@gmail.com>
2023-02-22 09:52:01 -0600

    Update tests/Testcontainers.Redpanda.Tests/Usings.cs
    Co-authored-by: Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>

--- tests/Testcontainers.Redpanda.Tests/Usings.cs
+++ tests/Testcontainers.Redpanda.Tests/Usings.cs
@@ -1,3 +1,6 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Collections.ObjectModel;
 global using System.Threading.Tasks;
 global using Confluent.Kafka;
 global using DotNet.Testcontainers.Commons;
