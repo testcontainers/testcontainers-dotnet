9e7599d0a90ad09e54c2bd55ab1bbba3402d588a
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2023-03-01 12:40:42 +0100

    feat: Add SQL Edge module

---
+++ tests/Testcontainers.SqlEdge.Tests/SqlEdgeContainerTest.cs
@@ -0,0 +1,30 @@
+﻿namespace Testcontainers.SqlEdge;
+
+public sealed class SqlEdgeContainerTest : IAsyncLifetime
+{
+    private readonly SqlEdgeContainer _sqlEdgeContainer = new SqlEdgeBuilder().Build();
+
+    public Task InitializeAsync()
+    {
+        return _sqlEdgeContainer.StartAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return _sqlEdgeContainer.DisposeAsync().AsTask();
+    }
+
+    [Fact]
+    [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+    public void ConnectionStateReturnsOpen()
+    {
+        // Given
+        using DbConnection connection = new SqlConnection(_sqlEdgeContainer.GetConnectionString());
+
+        // When
+        connection.Open();
+
+        // Then
+        Assert.Equal(ConnectionState.Open, connection.State);
+    }
+}
