09342929b390e0dddc54a82a045878558491f6c8
Shahar <shaharrefaeli61@gmail.com>
2023-03-03 18:22:05 +0200

    Add webDriverType
    It is describes all selenium images in state of standalone and the video recording

---
+++ src/Testcontainers.WebDriver/WebDriverType.cs
@@ -0,0 +1,20 @@
+﻿namespace Testcontainers.WebDriver;
+
+public readonly struct WebDriverType
+{
+    public static WebDriverType Chrome => new WebDriverType("chrome", new DockerImage("selenium/standalone-chrome"));
+    public static WebDriverType Firefox => new WebDriverType("firefox", new DockerImage("selenium/standalone-firefox"));
+    public static WebDriverType MicrosoftEdge => new WebDriverType("MicrosoftEdge", new DockerImage("selenium/standalone-edge"));
+    public static WebDriverType Opera => new WebDriverType("opera", new DockerImage("selenium/standalone-opera"));
+    public static WebDriverType Video => new WebDriverType("ffmpeg", new DockerImage("selenium/video"));
+
+    private WebDriverType(string name, IImage image)
+    {
+        Name = name;
+        Image = image;
+    }
+
+    public string Name { get; }
+
+    public IImage Image { get; }
+}
