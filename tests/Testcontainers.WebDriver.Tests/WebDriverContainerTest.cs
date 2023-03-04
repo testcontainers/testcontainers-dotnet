a0e411cd2011b1f6fd4f20a20d51e27e2808254b
Shahar <shaharrefaeli61@gmail.com>
2023-03-04 19:15:35 +0200

    Add container of hello world and find title element

--- tests/Testcontainers.WebDriver.Tests/WebDriverContainerTest.cs
+++ tests/Testcontainers.WebDriver.Tests/WebDriverContainerTest.cs
@@ -2,21 +2,44 @@ namespace Testcontainers.WebDriver;
 
 public class WebDriverContainerTest : IAsyncLifetime
 {
-    private readonly WebDriverContainer _webDriverContainer = new WebDriverBuilder().Build();
+  private readonly WebDriverContainer _webDriverContainer = new WebDriverBuilder()
+    .WithBrowser(WebDriverType.Chrome)
+    .Build();
 
-    public Task InitializeAsync()
-    {
-        return this._webDriverContainer.StartAsync();
-    }
+  private const ushort UiPort = 8080;
+  private readonly IContainer _testcontainersHelloworld = new ContainerBuilder()
+    .WithImage("testcontainers/helloworld")
+    .WithPortBinding(UiPort, true)
+    .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(UiPort))
+    .Build();
 
-    public Task DisposeAsync()
-    {
-        return this._webDriverContainer.DisposeAsync().AsTask();
-    }
+  public Task InitializeAsync()
+  {
+    this._testcontainersHelloworld.StartAsync();
+    return this._webDriverContainer.StartAsync();
+  }
 
-    [Fact]
-    [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
-    public void CreateContainer()
-    {
-    }
+  public Task DisposeAsync()
+  {
+    return this._webDriverContainer.DisposeAsync().AsTask();
+  }
+
+  [Fact]
+  [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+  public void CreateContainer()
+  {
+    // Given
+    var remoteWebDriver = new RemoteWebDriver(this._webDriverContainer.GetWebDriverUri(), new ChromeOptions());
+
+    // Then
+    var helloWorldUri = new UriBuilder(Uri.UriSchemeHttp,
+      this._testcontainersHelloworld.Hostname,
+          this._testcontainersHelloworld.GetMappedPublicPort(UiPort)).Uri;
+
+    remoteWebDriver.Navigate().GoToUrl(helloWorldUri);
+    var title = remoteWebDriver.FindElementByTagName("h1").Text;
+
+    // Then
+    Assert.Equal("Hello world", title);
+  }
 }
