4c13b28e424f07b9cdd41ec304e6d04c0b4b3de5
Daniel Boos <JejuniProgramming@gmail.com>
2023-03-29 12:31:41 +0200

    feat: made WithUsername private again

--- src/Testcontainers.MsSql/MsSqlBuilder.cs
+++ src/Testcontainers.MsSql/MsSqlBuilder.cs
@@ -62,20 +62,6 @@ public sealed class MsSqlBuilder : ContainerBuilder<MsSqlBuilder, MsSqlContainer
             .WithEnvironment("SQLCMDDBNAME", database);
     }
 
-    /// <summary>
-    /// Sets the MsSql username.
-    /// </summary>
-    /// <remarks>
-    /// The Docker image does not allow to configure the username.
-    /// </remarks>
-    /// <param name="username">The MsSql username.</param>
-    /// <returns>A configured instance of <see cref="MsSqlBuilder" />.</returns>
-    public MsSqlBuilder WithUsername(string username)
-    {
-        return Merge(DockerResourceConfiguration, new MsSqlConfiguration(username: username))
-            .WithEnvironment("SQLCMDUSER", username);
-    }
-
     /// <inheritdoc />
     public override MsSqlContainer Build()
     {
@@ -122,6 +108,20 @@ public sealed class MsSqlBuilder : ContainerBuilder<MsSqlBuilder, MsSqlContainer
     protected override MsSqlBuilder Merge(MsSqlConfiguration oldValue, MsSqlConfiguration newValue)
     {
         return new MsSqlBuilder(new MsSqlConfiguration(oldValue, newValue));
+    }
+
+    /// <summary>
+    /// Sets the MsSql username.
+    /// </summary>
+    /// <remarks>
+    /// The Docker image does not allow to configure the username.
+    /// </remarks>
+    /// <param name="username">The MsSql username.</param>
+    /// <returns>A configured instance of <see cref="MsSqlBuilder" />.</returns>
+    private MsSqlBuilder WithUsername(string username)
+    {
+        return Merge(DockerResourceConfiguration, new MsSqlConfiguration(username: username))
+            .WithEnvironment("SQLCMDUSER", username);
     }
 
     /// <inheritdoc cref="IWaitUntil" />
