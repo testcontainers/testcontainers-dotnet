5039557856ad2e8b79fe501ee8b963f16f8f108c
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2023-04-19 08:29:26 +0200

    chore: Still pull from Docker Hub

--- src/Testcontainers/Containers/ResourceReaper.cs
+++ src/Testcontainers/Containers/ResourceReaper.cs
@@ -33,7 +33,7 @@ namespace DotNet.Testcontainers.Containers
     /// </summary>
     private const int RetryTimeoutInSeconds = 2;
 
-    private static readonly IImage RyukImage = new DockerImage("registry.testcontainers.com/testcontainers/ryuk:0.4.0");
+    private static readonly IImage RyukImage = new DockerImage("testcontainers/ryuk:0.4.0");
 
     private static readonly SemaphoreSlim DefaultLock = new SemaphoreSlim(1, 1);
 
