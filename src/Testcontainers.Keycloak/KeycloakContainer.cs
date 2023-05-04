adb94d62e9ff488a83d46cee79693a4919f0d5f1
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2023-05-04 08:26:56 +0300

    feat: Add Keycloak module

---
+++ src/Testcontainers.Keycloak/KeycloakContainer.cs
@@ -0,0 +1,33 @@
+﻿namespace Testcontainers.Keycloak;
+
+/// <inheritdoc />
+[PublicAPI]
+public sealed class KeycloakContainer : DockerContainer
+{
+	private readonly KeycloakConfiguration _configuration;
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="KeycloakContainer"/> class.
+	/// </summary>
+	/// <param name="configuration">The container configuration.</param>
+	/// <param name="logger">The logger.</param>
+	public KeycloakContainer(KeycloakConfiguration configuration, ILogger logger)
+		: base(configuration, logger)
+	{
+		_configuration = configuration;
+	}
+
+	/// <summary>
+	/// Gets the base address for this Keycloak instance.
+	/// </summary>
+	/// <returns>The base address for this Keycloak instance</returns>
+	public Uri GetBaseAddress()
+	{
+		return new UriBuilder
+		{
+			Scheme = "http",
+			Host = Hostname,
+			Port = GetMappedPublicPort(KeycloakBuilder.KeycloakPort)
+		}.Uri;
+	}
+}
