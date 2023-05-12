36ded55005230c39cf6e3dd51b0e398c84023efe
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2023-05-12 11:50:46 +0300

    feat: Add Keycloak module (#880)
    Co-authored-by: Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>

---
+++ tests/Testcontainers.Keycloak.Tests/KeycloakContainerTest.cs
@@ -0,0 +1,45 @@
+namespace Testcontainers.Keycloak.Tests;
+
+public sealed class KeycloakContainerTest : IAsyncLifetime
+{
+    private readonly KeycloakContainer _keycloakContainer = new KeycloakBuilder().Build();
+
+    public Task InitializeAsync()
+    {
+        return _keycloakContainer.StartAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return _keycloakContainer.DisposeAsync().AsTask();
+    }
+
+    [Fact]
+    public async Task GetOpenIdEndpointReturnsHttpStatusCodeOk()
+    {
+        // Given
+        using var httpClient = new HttpClient();
+        httpClient.BaseAddress = new Uri(_keycloakContainer.GetBaseAddress());
+
+        // When
+        using var response = await httpClient.GetAsync("/realms/master/.well-known/openid-configuration")
+            .ConfigureAwait(false);
+
+        // Then
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task MasterRealmIsEnabled()
+    {
+        // Given
+        var keycloakClient = new KeycloakClient(_keycloakContainer.GetBaseAddress(), KeycloakBuilder.DefaultUsername, KeycloakBuilder.DefaultPassword);
+
+        // When
+        var masterRealm = await keycloakClient.GetRealmAsync("master")
+            .ConfigureAwait(false);
+
+        // Then
+        Assert.True(masterRealm.Enabled);
+    }
+}
