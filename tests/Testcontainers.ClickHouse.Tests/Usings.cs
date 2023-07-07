6dafc8efbe1e70492a22a1446bc11edd19ea2bf1
Shaun Becker <smbecker@gmail.com>
2023-07-07 17:46:39 -0400

    Add support for clickhouse container

---
+++ tests/Testcontainers.ClickHouse.Tests/Usings.cs
@@ -0,0 +1,6 @@
+global using System.Data;
+global using System.Data.Common;
+global using System.Threading.Tasks;
+global using ClickHouse.Client.ADO;
+global using DotNet.Testcontainers.Commons;
+global using Xunit;
