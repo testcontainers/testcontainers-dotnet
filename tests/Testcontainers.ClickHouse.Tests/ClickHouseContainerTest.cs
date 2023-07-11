5c151457be490e7eff497c3df71d21881936e923
Shaun Becker <smbecker@gmail.com>
2023-07-11 04:08:54 -0400

    feat: Add ClickHouse module (#944)
    Co-authored-by: Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>

---
+++ tests/Testcontainers.ClickHouse.Tests/ClickHouseContainerTest.cs
@@ -0,0 +1,45 @@
+namespace Testcontainers.ClickHouse;
+
+public sealed class ClickHouseContainerTest : IAsyncLifetime
+{
+    private readonly ClickHouseContainer _clickHouseContainer = new ClickHouseBuilder().Build();
+
+    public Task InitializeAsync()
+    {
+        return _clickHouseContainer.StartAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return _clickHouseContainer.DisposeAsync().AsTask();
+    }
+
+    [Fact]
+    [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+    public void ConnectionStateReturnsOpen()
+    {
+        // Given
+        using DbConnection connection = new ClickHouseConnection(_clickHouseContainer.GetConnectionString());
+
+        // When
+        connection.Open();
+
+        // Then
+        Assert.Equal(ConnectionState.Open, connection.State);
+    }
+
+    [Fact]
+    [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+    public async Task ExecScriptReturnsSuccessful()
+    {
+        // Given
+        const string scriptContent = "SELECT 1;";
+
+        // When
+        var execResult = await _clickHouseContainer.ExecScriptAsync(scriptContent)
+            .ConfigureAwait(false);
+
+        // When
+        Assert.True(0L.Equals(execResult.ExitCode), execResult.Stderr);
+    }
+}
