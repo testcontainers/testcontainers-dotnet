ca7850033aed3c27329571ea68c4f8ef0ebc4a83
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2023-08-03 19:53:49 +0200

    chore: Order usings

--- tests/Testcontainers.Kusto.Tests/Usings.cs
+++ tests/Testcontainers.Kusto.Tests/Usings.cs
@@ -1,6 +1,6 @@
 global using System.Threading.Tasks;
 global using DotNet.Testcontainers.Commons;
-global using Xunit;
 global using Kusto.Data;
 global using Kusto.Data.Common;
 global using Kusto.Data.Net.Client;
+global using Xunit;
