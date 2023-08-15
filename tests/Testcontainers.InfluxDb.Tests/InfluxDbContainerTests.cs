5d9ee3d875ff5130fd9b0a6b1942c592a134a38f
MelomanG <hubert.golaszewski99@gmail.com>
2023-08-15 11:53:18 +0200

    add InfluxDb test container tests

---
+++ tests/Testcontainers.InfluxDb.Tests/InfluxDbContainerTests.cs
@@ -0,0 +1,33 @@
+﻿using InfluxDB.Client;
+
+namespace Testcontainers.InfluxDb.Tests;
+
+public sealed class InfluxDbContainerTest : IAsyncLifetime
+{
+    private readonly InfluxDbContainer _influxDbContainer = new InfluxDbBuilder().Build();
+
+    public Task InitializeAsync()
+    {
+        return _influxDbContainer.StartAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return _influxDbContainer.DisposeAsync().AsTask();
+    }
+
+    [Fact]
+    [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+    public async Task PingReturnsTrue()
+    {
+        // Given
+        using var client = new InfluxDBClient(_influxDbContainer.GetAddress());
+
+        // When
+        var result = await client.PingAsync();
+
+        // Then
+        Assert.True(result);
+    }
+}
+
