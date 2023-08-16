1d06f51f664c053854a28561393f736b07de0a85
MelomanG <hubert.golaszewski99@gmail.com>
2023-08-16 18:43:06 +0200

    add more complex test

--- tests/Testcontainers.InfluxDb.Tests/InfluxDbContainerTests.cs
+++ tests/Testcontainers.InfluxDb.Tests/InfluxDbContainerTests.cs
@@ -2,7 +2,16 @@
 
 public sealed class InfluxDbContainerTest : IAsyncLifetime
 {
-    private readonly InfluxDbContainer _influxDbContainer = new InfluxDbBuilder().Build();
+    private const string Token = "test_admin_token";
+    private const string Organization = "test_organization";
+    private const string Bucket = "test_bucket";
+
+    private readonly InfluxDbContainer _influxDbContainer =
+        new InfluxDbBuilder()
+            .WithAdminToken(Token)
+            .WithOrganization(Organization)
+            .WithBucket(Bucket)
+            .Build();
 
     public Task InitializeAsync()
     {
@@ -19,12 +28,43 @@ public sealed class InfluxDbContainerTest : IAsyncLifetime
     public async Task PingReturnsTrue()
     {
         // Given
-        using var client = new InfluxDBClient(_influxDbContainer.GetAddress());
+        using var client = new InfluxDBClient(_influxDbContainer.GetAddress(), Token);
 
         // When
         var result = await client.PingAsync();
 
         // Then
         Assert.True(result);
+    }
+
+    [Fact]
+    [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+    public async Task PointQueryReturnsWrittenPoint()
+    {
+        // Given
+        const string expectedLocationTag = "location";
+        const string expectedLocationTagValue = "west";
+        const double fieldValue = 55D;
+        const string query = $"from(bucket:\"{Bucket}\") |> range(start: 0)";
+
+        using var client = new InfluxDBClient(_influxDbContainer.GetAddress(), Token);
+        var writeApi = client.GetWriteApiAsync();
+        var queryApi = client.GetQueryApi();
+
+        var point = PointData
+            .Measurement("temperature")
+            .Tag(expectedLocationTag, expectedLocationTagValue)
+            .Field("value", fieldValue)
+            .Timestamp(DateTime.UtcNow.AddSeconds(-10), WritePrecision.Ns);
+
+        // When
+        await writeApi.WritePointAsync(point, Bucket, Organization);
+        var fluxTables = await queryApi.QueryAsync(query, Organization);
+
+        // Then
+        var recordValues = fluxTables.SingleOrDefault()?.Records?.SingleOrDefault()?.Values;
+        Assert.NotNull(recordValues);
+        Assert.Equal(expectedLocationTagValue, recordValues[expectedLocationTag]);
+        Assert.Equal(fieldValue, recordValues["_value"]);
     }
 }
