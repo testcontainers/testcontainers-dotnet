27611d1967b9795cb457c0438d75ee3327163a39
Chris Philips <the.avid.engineer@outlook.com>
2023-08-28 15:24:00 -0700

    chore: update doccomment

--- src/Testcontainers.MongoDb/MongoDbBuilder.cs
+++ src/Testcontainers.MongoDb/MongoDbBuilder.cs
@@ -17,6 +17,7 @@ public sealed class MongoDbBuilder : ContainerBuilder<MongoDbBuilder, MongoDbCon
     /// <summary>
     /// Initializes a new instance of the <see cref="MongoDbBuilder" /> class.
     /// </summary>
+    /// <param name="useDefaultCredentials">When set to <c>false</c>, mongodb will run without authentication unless you explicitly specify credentials</param>
     public MongoDbBuilder(bool useDefaultCredentials = true)
         : this(new MongoDbConfiguration())
     {
