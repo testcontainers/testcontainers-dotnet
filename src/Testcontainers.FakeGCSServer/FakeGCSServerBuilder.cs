37d15609a53fff274e8ca1c22ef0036350560eb9
Konstantin Semenenko <mail@ksemenenko.com>
2023-10-16 13:41:32 +0200

    global using

--- src/Testcontainers.FakeGCSServer/FakeGCSServerBuilder.cs
+++ src/Testcontainers.FakeGCSServer/FakeGCSServerBuilder.cs
@@ -1,6 +1,3 @@
-using System.Text;
-using System.Text.RegularExpressions;
-
 namespace Testcontainers.FakeGCSServer;
 
 /// <inheritdoc cref="ContainerBuilder{TBuilderEntity, TContainerEntity, TConfigurationEntity}" />
--- src/Testcontainers.FakeGCSServer/Usings.cs
+++ src/Testcontainers.FakeGCSServer/Usings.cs
@@ -6,3 +6,5 @@ global using DotNet.Testcontainers.Configurations;
 global using DotNet.Testcontainers.Containers;
 global using JetBrains.Annotations;
 global using Microsoft.Extensions.Logging;
+global using System.Text;
+global using System.Text.RegularExpressions;
