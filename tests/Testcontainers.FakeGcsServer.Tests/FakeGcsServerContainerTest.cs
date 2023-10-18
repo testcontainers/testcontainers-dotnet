b74532683808f9cd895d1562957ce2493ea3dcf6
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2023-10-18 20:23:21 +0200

    chore: Rename test member

--- tests/Testcontainers.FakeGcsServer.Tests/FakeGcsServerContainerTest.cs
+++ tests/Testcontainers.FakeGcsServer.Tests/FakeGcsServerContainerTest.cs
@@ -14,44 +14,41 @@ public abstract class FakeGcsServerContainerTest : IAsyncLifetime
         return _fakeGcsServerContainer.DisposeAsync().AsTask();
     }
 
-    public sealed class BlobService : FakeGcsServerContainerTest
+    [Fact]
+    [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+    public async Task DownloadObjectReturnsUploadObject()
     {
-        [Fact]
-        [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
-        public async Task EstablishesConnection()
-        {
-            // Given
-            const string helloWorld = "Hello, World!";
+        // Given
+        const string helloWorld = "Hello, World!";
 
-            using var writeStream = new MemoryStream(Encoding.Default.GetBytes(helloWorld));
+        using var writeStream = new MemoryStream(Encoding.Default.GetBytes(helloWorld));
 
-            using var readStream = new MemoryStream();
+        using var readStream = new MemoryStream();
 
-            var project = Guid.NewGuid().ToString("D");
+        var project = Guid.NewGuid().ToString("D");
 
-            var bucket = Guid.NewGuid().ToString("D");
+        var bucket = Guid.NewGuid().ToString("D");
 
-            var fileName = Path.GetRandomFileName();
+        var fileName = Path.GetRandomFileName();
 
-            var storageClientBuilder = new StorageClientBuilder();
-            storageClientBuilder.UnauthenticatedAccess = true;
-            storageClientBuilder.BaseUri = _fakeGcsServerContainer.GetConnectionString();
+        var storageClientBuilder = new StorageClientBuilder();
+        storageClientBuilder.UnauthenticatedAccess = true;
+        storageClientBuilder.BaseUri = _fakeGcsServerContainer.GetConnectionString();
 
-            // When
-            var client = await storageClientBuilder.BuildAsync()
-                .ConfigureAwait(false);
+        // When
+        var client = await storageClientBuilder.BuildAsync()
+            .ConfigureAwait(false);
 
-            _ = await client.CreateBucketAsync(project, bucket)
-                .ConfigureAwait(false);
+        _ = await client.CreateBucketAsync(project, bucket)
+            .ConfigureAwait(false);
 
-            _ = await client.UploadObjectAsync(bucket, fileName, "text/plain", writeStream)
-                .ConfigureAwait(false);
+        _ = await client.UploadObjectAsync(bucket, fileName, "text/plain", writeStream)
+            .ConfigureAwait(false);
 
-            _ = await client.DownloadObjectAsync(bucket, fileName, readStream)
-                .ConfigureAwait(false);
+        _ = await client.DownloadObjectAsync(bucket, fileName, readStream)
+            .ConfigureAwait(false);
 
-            // Then
-            Assert.Equal(helloWorld, Encoding.Default.GetString(readStream.ToArray()));
-        }
+        // Then
+        Assert.Equal(helloWorld, Encoding.Default.GetString(readStream.ToArray()));
     }
 }
