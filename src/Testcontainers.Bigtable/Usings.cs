dcc5d5a10c773607b8afeb7eebdd04b4ef559770
Per Øyvind Håvelsrud <per.oyvind.havelsrud@elvia.no>
2023-11-09 09:15:39 +0100

    Add .editorconfig file that was missing, and remove unnecesarry whitespace in solution and project files.

--- src/Testcontainers.Bigtable/Usings.cs
+++ src/Testcontainers.Bigtable/Usings.cs
@@ -1,6 +1,3 @@
-global using System;
-global using System.Collections.Generic;
-global using System.Linq;
 global using Docker.DotNet.Models;
 global using DotNet.Testcontainers.Builders;
 global using DotNet.Testcontainers.Configurations;
