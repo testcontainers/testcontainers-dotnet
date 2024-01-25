8f548e43ef45c3d4055a88d542db445c036505f9
Frank R. Haugen <frank.haugen@gmail.com>
2024-01-25 11:56:50 +0100

    Added test project for Ollama module

---
+++ tests/Testcontainers.Ollama/UnitTest1.cs
@@ -0,0 +1,19 @@
+namespace Testcontainers.Ollama;
+
+public class OllamaContainerTests : IAsyncLifetime
+{
+  [Fact]
+  public void Test1()
+  {
+  }
+
+  /// <inheritdoc />
+  public async Task InitializeAsync()
+  {
+  }
+
+  /// <inheritdoc />
+  public async Task DisposeAsync()
+  {
+  }
+}
