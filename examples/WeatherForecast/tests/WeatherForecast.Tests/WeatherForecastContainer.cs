ef1b3277503ab120931082be01ffd1e347025dd5
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2024-02-03 20:32:39 +0100

    chore: Switch to the SDK's included Central Package Management

--- examples/WeatherForecast/tests/WeatherForecast.Tests/WeatherForecastContainer.cs
+++ examples/WeatherForecast/tests/WeatherForecast.Tests/WeatherForecastContainer.cs
@@ -22,7 +22,7 @@ public sealed class WeatherForecastContainer : HttpClient, IAsyncLifetime
   {
     const string weatherForecastStorage = "weatherForecastStorage";
 
-    const string connectionString = $"server={weatherForecastStorage};user id={SqlEdgeBuilder.DefaultUsername};password={SqlEdgeBuilder.DefaultPassword};database={SqlEdgeBuilder.DefaultDatabase}";
+    const string connectionString = $"Server={weatherForecastStorage};User Id={SqlEdgeBuilder.DefaultUsername};Password={SqlEdgeBuilder.DefaultPassword};Database={SqlEdgeBuilder.DefaultDatabase};TrustServerCertificate=True";
 
     _weatherForecastNetwork = new NetworkBuilder()
       .Build();
