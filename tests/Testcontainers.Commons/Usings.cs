28df26e04d96af0ab419ae6330d32f970c5dd520
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2024-02-10 22:32:27 +0100

    fix: Consider the timestamp in the log message wait strategy (read the correct log message chunk) (#1110)

--- tests/Testcontainers.Commons/Usings.cs
+++ tests/Testcontainers.Commons/Usings.cs
@@ -2,5 +2,8 @@ global using System;
 global using System.Diagnostics;
 global using System.IO;
 global using System.Text;
+global using System.Threading.Tasks;
+global using DotNet.Testcontainers.Containers;
 global using DotNet.Testcontainers.Images;
 global using JetBrains.Annotations;
+global using Xunit;
