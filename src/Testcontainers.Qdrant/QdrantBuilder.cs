331abc561085ae13790f94af3b46453e1dc7cb17
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2024-04-07 09:38:18 +0200

    chore: Fix minor repo inconsistencies

--- src/Testcontainers.Qdrant/QdrantBuilder.cs
+++ src/Testcontainers.Qdrant/QdrantBuilder.cs
@@ -1,97 +1,94 @@
-using System.Linq;
-using System.Net.Http;
-
 namespace Testcontainers.Qdrant;
 
 /// <inheritdoc cref="ContainerBuilder{TBuilderEntity, TContainerEntity, TConfigurationEntity}" />
 [PublicAPI]
 public sealed class QdrantBuilder : ContainerBuilder<QdrantBuilder, QdrantContainer, QdrantConfiguration>
 {
-	public const string QdrantImage = "qdrant/qdrant:v1.8.3";
+    public const string QdrantImage = "qdrant/qdrant:v1.8.3";
 
-	public const ushort QdrantHttpPort = 6333;
+    public const ushort QdrantHttpPort = 6333;
 
-	public const ushort QdrantGrpcPort = 6334;
-	
-	public const string QdrantTlsCertFilePath = "/qdrant/tls/cert.pem";
-	
-	public const string QdrantTlsKeyFilePath = "/qdrant/tls/key.pem";
+    public const ushort QdrantGrpcPort = 6334;
 
-	public QdrantBuilder() : this(new QdrantConfiguration()) =>
-		DockerResourceConfiguration = Init().DockerResourceConfiguration;
+    public const string QdrantTlsCertFilePath = "/qdrant/tls/cert.pem";
 
-	private QdrantBuilder(QdrantConfiguration dockerResourceConfiguration) : base(dockerResourceConfiguration) =>
-		DockerResourceConfiguration = dockerResourceConfiguration;
-	
-	/// <summary>
-	/// The API key used to secure the instance. A certificate and private key should also be
-	/// provided to <see cref="WithCertificate"/> to enable Transport Layer Security (TLS).
-	/// </summary>
-	/// <param name="apiKey">The API key</param>
-	public QdrantBuilder WithApiKey(string apiKey) =>
-		Merge(DockerResourceConfiguration, new QdrantConfiguration(apiKey: apiKey))
-			.WithEnvironment("QDRANT__SERVICE__API_KEY", apiKey);
+    public const string QdrantTlsKeyFilePath = "/qdrant/tls/key.pem";
 
-	/// <summary>
-	/// A certificate and private key to enable Transport Layer Security (TLS).
-	/// </summary>
-	/// <param name="certificate">A public certificate in PEM format</param>
-	/// <param name="privateKey">A private key for the certificate in PEM format</param>
-	public QdrantBuilder WithCertificate(string certificate, string privateKey)
-	{
-		return Merge(DockerResourceConfiguration, new QdrantConfiguration(certificate: certificate, privateKey: privateKey))
-			.WithEnvironment("QDRANT__SERVICE__ENABLE_TLS", "1")
-			.WithResourceMapping(Encoding.UTF8.GetBytes(certificate), QdrantTlsCertFilePath)
-			.WithEnvironment("QDRANT__TLS__CERT", QdrantTlsCertFilePath)
-			.WithResourceMapping(Encoding.UTF8.GetBytes(privateKey), QdrantTlsKeyFilePath)
-			.WithEnvironment("QDRANT__TLS__KEY", QdrantTlsKeyFilePath);
-	}
-    
-	/// <inheritdoc />
-	public override QdrantContainer Build()
-	{
-		Validate();
+    public QdrantBuilder() : this(new QdrantConfiguration()) =>
+        DockerResourceConfiguration = Init().DockerResourceConfiguration;
 
-		var waitStrategy = Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(request =>
-		{
-			var httpWaitStrategy = request.ForPort(QdrantHttpPort).ForPath("/readyz");
-			
-			// allow any certificate defined to pass validation
-			if (DockerResourceConfiguration.Certificate is not null)
-			{
-				httpWaitStrategy.UsingTls()
-					.UsingHttpMessageHandler(new HttpClientHandler
-					{
-						ServerCertificateCustomValidationCallback = (_, _, _, _) => true 
-					});
-			}
+    private QdrantBuilder(QdrantConfiguration dockerResourceConfiguration) : base(dockerResourceConfiguration) =>
+        DockerResourceConfiguration = dockerResourceConfiguration;
 
-			return httpWaitStrategy;
-		});
-		
-		var qdrantBuilder = DockerResourceConfiguration.WaitStrategies.Count() > 1 ? this : WithWaitStrategy(waitStrategy);
-		return new QdrantContainer(qdrantBuilder.DockerResourceConfiguration);
-	}
+    /// <summary>
+    /// The API key used to secure the instance. A certificate and private key should also be
+    /// provided to <see cref="WithCertificate"/> to enable Transport Layer Security (TLS).
+    /// </summary>
+    /// <param name="apiKey">The API key</param>
+    public QdrantBuilder WithApiKey(string apiKey) =>
+        Merge(DockerResourceConfiguration, new QdrantConfiguration(apiKey: apiKey))
+            .WithEnvironment("QDRANT__SERVICE__API_KEY", apiKey);
 
-	/// <inheritdoc />
-	protected override QdrantBuilder Init() =>
-		base.Init()
-			.WithImage(QdrantImage)
-			.WithPortBinding(QdrantHttpPort, true)
-			.WithPortBinding(QdrantGrpcPort, true);
+    /// <summary>
+    /// A certificate and private key to enable Transport Layer Security (TLS).
+    /// </summary>
+    /// <param name="certificate">A public certificate in PEM format</param>
+    /// <param name="privateKey">A private key for the certificate in PEM format</param>
+    public QdrantBuilder WithCertificate(string certificate, string privateKey)
+    {
+        return Merge(DockerResourceConfiguration, new QdrantConfiguration(certificate: certificate, privateKey: privateKey))
+            .WithEnvironment("QDRANT__SERVICE__ENABLE_TLS", "1")
+            .WithResourceMapping(Encoding.UTF8.GetBytes(certificate), QdrantTlsCertFilePath)
+            .WithEnvironment("QDRANT__TLS__CERT", QdrantTlsCertFilePath)
+            .WithResourceMapping(Encoding.UTF8.GetBytes(privateKey), QdrantTlsKeyFilePath)
+            .WithEnvironment("QDRANT__TLS__KEY", QdrantTlsKeyFilePath);
+    }
 
-	/// <inheritdoc />
-	protected override QdrantBuilder Clone(IResourceConfiguration<CreateContainerParameters> resourceConfiguration) =>
-		Merge(DockerResourceConfiguration, new QdrantConfiguration(resourceConfiguration));
+    /// <inheritdoc />
+    public override QdrantContainer Build()
+    {
+        Validate();
 
-	/// <inheritdoc />
-	protected override QdrantBuilder Merge(QdrantConfiguration oldValue, QdrantConfiguration newValue) =>
-		new(new QdrantConfiguration(oldValue, newValue));
+        var waitStrategy = Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(request =>
+        {
+            var httpWaitStrategy = request.ForPort(QdrantHttpPort).ForPath("/readyz");
 
-	/// <inheritdoc />
-	protected override QdrantConfiguration DockerResourceConfiguration { get; }
+            // allow any certificate defined to pass validation
+            if (DockerResourceConfiguration.Certificate is not null)
+            {
+                httpWaitStrategy.UsingTls()
+                    .UsingHttpMessageHandler(new HttpClientHandler
+                    {
+                        ServerCertificateCustomValidationCallback = (_, _, _, _) => true
+                    });
+            }
 
-	/// <inheritdoc />
-	protected override QdrantBuilder Clone(IContainerConfiguration resourceConfiguration) =>
-		Merge(DockerResourceConfiguration, new QdrantConfiguration(resourceConfiguration));
+            return httpWaitStrategy;
+        });
+
+        var qdrantBuilder = DockerResourceConfiguration.WaitStrategies.Count() > 1 ? this : WithWaitStrategy(waitStrategy);
+        return new QdrantContainer(qdrantBuilder.DockerResourceConfiguration);
+    }
+
+    /// <inheritdoc />
+    protected override QdrantBuilder Init() =>
+        base.Init()
+            .WithImage(QdrantImage)
+            .WithPortBinding(QdrantHttpPort, true)
+            .WithPortBinding(QdrantGrpcPort, true);
+
+    /// <inheritdoc />
+    protected override QdrantBuilder Clone(IResourceConfiguration<CreateContainerParameters> resourceConfiguration) =>
+        Merge(DockerResourceConfiguration, new QdrantConfiguration(resourceConfiguration));
+
+    /// <inheritdoc />
+    protected override QdrantBuilder Merge(QdrantConfiguration oldValue, QdrantConfiguration newValue) =>
+        new(new QdrantConfiguration(oldValue, newValue));
+
+    /// <inheritdoc />
+    protected override QdrantConfiguration DockerResourceConfiguration { get; }
+
+    /// <inheritdoc />
+    protected override QdrantBuilder Clone(IContainerConfiguration resourceConfiguration) =>
+        Merge(DockerResourceConfiguration, new QdrantConfiguration(resourceConfiguration));
 }
--- src/Testcontainers.Qdrant/QdrantConfiguration.cs
+++ src/Testcontainers.Qdrant/QdrantConfiguration.cs
@@ -1,73 +1,71 @@
-using static DotNet.Testcontainers.Builders.BuildConfiguration;
-
 namespace Testcontainers.Qdrant;
 
 /// <inheritdoc cref="ContainerConfiguration" />
 [PublicAPI]
 public sealed class QdrantConfiguration : ContainerConfiguration
 {
-	/// <summary>
-	/// Initializes a new instance of the <see cref="QdrantConfiguration" /> class.
-	/// </summary>
-	public QdrantConfiguration(string apiKey = null, string certificate = null, string privateKey = null)
-	{
-		ApiKey = apiKey;
-		Certificate = certificate;
-		PrivateKey = privateKey;
-	}
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QdrantConfiguration" /> class.
+    /// </summary>
+    public QdrantConfiguration(string apiKey = null, string certificate = null, string privateKey = null)
+    {
+        ApiKey = apiKey;
+        Certificate = certificate;
+        PrivateKey = privateKey;
+    }
 
-	/// <summary>
-	/// Initializes a new instance of the <see cref="QdrantConfiguration" /> class.
-	/// </summary>
-	/// <param name="resourceConfiguration">The Docker resource configuration.</param>
-	public QdrantConfiguration(IResourceConfiguration<CreateContainerParameters> resourceConfiguration)
-		: base(resourceConfiguration)
-	{
-	}
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QdrantConfiguration" /> class.
+    /// </summary>
+    /// <param name="resourceConfiguration">The Docker resource configuration.</param>
+    public QdrantConfiguration(IResourceConfiguration<CreateContainerParameters> resourceConfiguration)
+        : base(resourceConfiguration)
+    {
+    }
 
-	/// <summary>
-	/// Initializes a new instance of the <see cref="QdrantConfiguration" /> class.
-	/// </summary>
-	/// <param name="resourceConfiguration">The Docker resource configuration.</param>
-	public QdrantConfiguration(IContainerConfiguration resourceConfiguration)
-		: base(resourceConfiguration)
-	{
-	}
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QdrantConfiguration" /> class.
+    /// </summary>
+    /// <param name="resourceConfiguration">The Docker resource configuration.</param>
+    public QdrantConfiguration(IContainerConfiguration resourceConfiguration)
+        : base(resourceConfiguration)
+    {
+    }
 
-	/// <summary>
-	/// Initializes a new instance of the <see cref="QdrantConfiguration" /> class.
-	/// </summary>
-	/// <param name="resourceConfiguration">The Docker resource configuration.</param>
-	public QdrantConfiguration(QdrantConfiguration resourceConfiguration)
-		: this(new QdrantConfiguration(), resourceConfiguration)
-	{
-	}
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QdrantConfiguration" /> class.
+    /// </summary>
+    /// <param name="resourceConfiguration">The Docker resource configuration.</param>
+    public QdrantConfiguration(QdrantConfiguration resourceConfiguration)
+        : this(new QdrantConfiguration(), resourceConfiguration)
+    {
+    }
 
-	/// <summary>
-	/// Initializes a new instance of the <see cref="QdrantConfiguration" /> class.
-	/// </summary>
-	/// <param name="oldValue">The old Docker resource configuration.</param>
-	/// <param name="newValue">The new Docker resource configuration.</param>
-	public QdrantConfiguration(QdrantConfiguration oldValue, QdrantConfiguration newValue)
-		: base(oldValue, newValue)
-	{
-		ApiKey = Combine(oldValue.ApiKey, newValue.ApiKey);
-		Certificate = Combine(oldValue.Certificate, newValue.Certificate);
-		PrivateKey = Combine(oldValue.PrivateKey, newValue.PrivateKey);
-	}
-	
-	/// <summary>
-	/// Gets the API key used to secure Qdrant.
-	/// </summary>
-	public string ApiKey { get; }
-	
-	/// <summary>
-	/// Gets the certificate used to configure Transport Layer Security. Certificate must be in PEM format.
-	/// </summary>
-	public string Certificate { get; }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QdrantConfiguration" /> class.
+    /// </summary>
+    /// <param name="oldValue">The old Docker resource configuration.</param>
+    /// <param name="newValue">The new Docker resource configuration.</param>
+    public QdrantConfiguration(QdrantConfiguration oldValue, QdrantConfiguration newValue)
+        : base(oldValue, newValue)
+    {
+        ApiKey = BuildConfiguration.Combine(oldValue.ApiKey, newValue.ApiKey);
+        Certificate = BuildConfiguration.Combine(oldValue.Certificate, newValue.Certificate);
+        PrivateKey = BuildConfiguration.Combine(oldValue.PrivateKey, newValue.PrivateKey);
+    }
 
-	/// <summary>
-	/// Gets the private key used to configure Transport Layer Security. Private key must be in PEM format.
-	/// </summary>
-	public string PrivateKey { get; }
+    /// <summary>
+    /// Gets the API key used to secure Qdrant.
+    /// </summary>
+    public string ApiKey { get; }
+
+    /// <summary>
+    /// Gets the certificate used to configure Transport Layer Security. Certificate must be in PEM format.
+    /// </summary>
+    public string Certificate { get; }
+
+    /// <summary>
+    /// Gets the private key used to configure Transport Layer Security. Private key must be in PEM format.
+    /// </summary>
+    public string PrivateKey { get; }
 }
