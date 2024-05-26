c3d144be33cccc4aa3487088bc0dc1f88b37fa9d
Robert Łysoń <robertlyson@gmail.com>
2024-05-26 20:53:10 +0200

    add testcontainers camunda

---
+++ src/Testcontainers.Camunda/CamundaContainer.cs
@@ -0,0 +1,14 @@
+﻿namespace Testcontainers.Camunda;
+
+/// <inheritdoc cref="DockerContainer" />
+[PublicAPI]
+public sealed class CamundaContainer : DockerContainer
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CamundaContainer" /> class.
+    /// </summary>
+    /// <param name="configuration">The container configuration.</param>
+    public CamundaContainer(IContainerConfiguration configuration) : base(configuration)
+    {
+    }
+}
