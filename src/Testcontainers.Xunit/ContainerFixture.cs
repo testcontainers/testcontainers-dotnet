39df2236d1634198e0a084119f2d40474b453f40
Cédric Luthi <cedric.luthi@gmail.com>
2024-05-27 23:04:02 +0200

    Share the lifetime implementation between ContainerFixture and ContainerTest

--- src/Testcontainers.Xunit/ContainerFixture.cs
+++ src/Testcontainers.Xunit/ContainerFixture.cs
@@ -8,61 +8,14 @@ namespace Testcontainers.Xunit;
 /// <typeparam name="TBuilderEntity">The builder entity.</typeparam>
 /// <typeparam name="TContainerEntity">The container entity.</typeparam>
 [PublicAPI]
-public class ContainerFixture<TBuilderEntity, TContainerEntity> : IAsyncLifetime
+public class ContainerFixture<TBuilderEntity, TContainerEntity>(IMessageSink messageSink) : ContainerLifetime<TBuilderEntity, TContainerEntity>
     where TBuilderEntity : IContainerBuilder<TBuilderEntity, TContainerEntity>, new()
     where TContainerEntity : IContainer
 {
-    private Lazy<TContainerEntity> _container;
-
-    public ContainerFixture(IMessageSink messageSink)
-    {
-        MessageSink = messageSink;
-        _container = new Lazy<TContainerEntity>(() =>
-        {
-            var containerBuilder = new TBuilderEntity().WithLogger(new MessageSinkLogger(MessageSink));
-            return Configure(containerBuilder).Build();
-        });
-    }
-
     /// <summary>
     /// The message sink used for reporting diagnostic messages.
     /// </summary>
-    protected IMessageSink MessageSink { get; }
+    protected IMessageSink MessageSink { get; } = messageSink;
 
-    /// <summary>
-    /// The container instance.
-    /// </summary>
-    public TContainerEntity Container => _container.Value;
-
-    /// <summary>
-    /// Extension point to further configure the container instance.
-    /// </summary>
-    /// <example>
-    /// <code>
-    /// public class MariaDbRootUserFixture(IMessageSink messageSink) : DbContainerFixture&lt;MariaDbBuilder, MariaDbContainer&gt;(messageSink)
-    /// {
-    ///   public override DbProviderFactory DbProviderFactory => MySqlConnectorFactory.Instance;
-    ///
-    ///   protected override MariaDbBuilder Configure(MariaDbBuilder builder)
-    ///   {
-    ///     return builder.WithUsername("root");
-    ///   }
-    /// }
-    /// </code>
-    /// </example>
-    /// <param name="builder">The container builder.</param>
-    /// <returns>A configured instance of <typeparamref name="TBuilderEntity" />.</returns>
-    protected virtual TBuilderEntity Configure(TBuilderEntity builder) => builder;
-
-    /// <inheritdoc />
-    Task IAsyncLifetime.InitializeAsync() => InitializeAsync();
-
-    /// <inheritdoc cref="IAsyncLifetime.InitializeAsync()" />
-    protected virtual Task InitializeAsync() => Container.StartAsync();
-
-    /// <inheritdoc />
-    Task IAsyncLifetime.DisposeAsync() => DisposeAsync();
-
-    /// <inheritdoc cref="IAsyncLifetime.DisposeAsync()" />
-    protected virtual Task DisposeAsync() => Container.DisposeAsync().AsTask();
+    protected override ILogger Logger { get; } = new MessageSinkLogger(messageSink);
 }
