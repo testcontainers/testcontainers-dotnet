eb4d5b99f86bda141800a87eec977366ee688ea2
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2024-09-02 06:37:14 +0200

    Add support for digest while creating containers

--- src/Testcontainers/Images/MatchImage.cs
+++ src/Testcontainers/Images/MatchImage.cs
@@ -5,9 +5,11 @@ namespace DotNet.Testcontainers.Images
 
   internal static class MatchImage
   {
-    private static readonly char[] SlashSeparator = { '/' };
+    private static readonly char[] AtSeparator = { '@' };
 
     private static readonly char[] ColonSeparator = { ':' };
+
+    private static readonly char[] SlashSeparator = { '/' };
 
     public static IImage Match(string image)
     {
@@ -19,16 +21,20 @@ namespace DotNet.Testcontainers.Images
 
       var registry = string.Join("/", imageComponents.Take(imageComponents.Length - 1));
 
-      imageComponents = imageComponents[imageComponents.Length - 1].Split(ColonSeparator, StringSplitOptions.RemoveEmptyEntries);
+      imageComponents = imageComponents[imageComponents.Length - 1].Split(AtSeparator, 2, StringSplitOptions.RemoveEmptyEntries);
+
+      var digest = imageComponents.Length == 2 ? $"@{imageComponents[1]}" : string.Empty;
+
+      imageComponents = imageComponents[0].Split(ColonSeparator, 2, StringSplitOptions.RemoveEmptyEntries);
 
       if (2.Equals(imageComponents.Length))
       {
-        return new DockerImage(registry, imageComponents[0], imageComponents[1]);
+        return new DockerImage(registry, imageComponents[0], string.IsNullOrEmpty(digest) ? imageComponents[1] : $"{imageComponents[1]}{digest}");
       }
 
       if (1.Equals(imageComponents.Length))
       {
-        return new DockerImage(registry, imageComponents[0], string.Empty);
+        return new DockerImage(registry, imageComponents[0], digest);
       }
 
       throw new ArgumentException("Cannot parse image: " + image, nameof(image));
--- tests/Testcontainers.Tests/Fixtures/Images/MatchImageFixture.cs
+++ tests/Testcontainers.Tests/Fixtures/Images/MatchImageFixture.cs
@@ -11,6 +11,8 @@ namespace DotNet.Testcontainers.Tests.Fixtures
       Add("test-image:test-tag", new DockerImage(string.Empty, "test-image", "test-tag"));
       Add("test-repository.com/test-image:test-tag", new DockerImage("test-repository.com", "test-image", "test-tag"));
       Add("test-parent-repository/test-child-repository/test-image:test-tag", new DockerImage("test-parent-repository/test-child-repository", "test-image", "test-tag"));
+      Add("test-parent-repository/test-child-repository/test-image@sha256:aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", new DockerImage("test-parent-repository/test-child-repository", "test-image", "@sha256:aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+      Add("test-parent-repository/test-child-repository/test-image:test-tag@sha256:aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", new DockerImage("test-parent-repository/test-child-repository", "test-image", "test-tag@sha256:aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
     }
   }
 }
