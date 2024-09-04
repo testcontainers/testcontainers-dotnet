f5898ec99351bd2a09de3739e4ddb17915c67129
Cédric Luthi <cedric.luthi@gmail.com>
2024-09-04 10:03:21 +0200

    Actually test reading the current context from Docker config files

--- tests/Testcontainers.Tests/Unit/Builders/DockerConfigTest.cs
+++ tests/Testcontainers.Tests/Unit/Builders/DockerConfigTest.cs
@@ -2,6 +2,9 @@ namespace DotNet.Testcontainers.Tests.Unit
 {
   using System;
   using System.IO;
+  using System.Runtime.CompilerServices;
+  using System.Security.Cryptography;
+  using System.Text;
   using DotNet.Testcontainers.Builders;
   using DotNet.Testcontainers.Commons;
   using DotNet.Testcontainers.Configurations;
@@ -12,31 +15,60 @@ namespace DotNet.Testcontainers.Tests.Unit
     public sealed class DockerContextConfigurationTests
     {
       [Fact]
-      public void ReturnsDefaultOsEndpointWhenDockerContextIsEmpty()
+      public void ReturnsActiveEndpointWhenDockerContextIsEmpty()
       {
         // Given
         ICustomConfiguration customConfiguration = new PropertiesFileConfiguration(new[] { "docker.context=" });
-        var dockerConfig = new DockerConfig(new FileInfo("config.json"), customConfiguration);
+        var dockerConfig = new DockerConfig(customConfiguration);
 
         // When
         var currentEndpoint = dockerConfig.GetCurrentEndpoint();
 
         // Then
-        Assert.Equal(TestcontainersSettings.OS.DockerEndpointAuthConfig.Endpoint, currentEndpoint);
+        Assert.Equal(DockerCli.GetCurrentEndpoint(), currentEndpoint);
       }
 
       [Fact]
-      public void ReturnsDefaultOsEndpointWhenDockerContextIsDefault()
+      public void ReturnsDefaultEndpointWhenDockerContextIsDefault()
       {
         // Given
         ICustomConfiguration customConfiguration = new PropertiesFileConfiguration(new[] { "docker.context=default" });
-        var dockerConfig = new DockerConfig(new FileInfo("config.json"), customConfiguration);
+        var dockerConfig = new DockerConfig(customConfiguration);
 
         // When
         var currentEndpoint = dockerConfig.GetCurrentEndpoint();
 
         // Then
-        Assert.Equal(TestcontainersSettings.OS.DockerEndpointAuthConfig.Endpoint, currentEndpoint);
+        Assert.Equal(DockerCli.GetCurrentEndpoint("default"), currentEndpoint);
+      }
+
+      [Fact]
+      public void ReturnsConfiguredEndpointWhenDockerContextIsCustomFromProperties()
+      {
+        // Given
+        using var context = new TempDockerContext("custom", "tcp://127.0.0.1:2375/");
+        ICustomConfiguration customConfiguration = new PropertiesFileConfiguration(new[] { "docker.context=custom" });
+        var dockerConfig = new DockerConfig(context.ConfigFile, customConfiguration);
+
+        // When
+        var currentEndpoint = dockerConfig.GetCurrentEndpoint();
+
+        // Then
+        Assert.Equal(new Uri("tcp://127.0.0.1:2375/"), currentEndpoint);
+      }
+
+      [Fact]
+      public void ReturnsConfiguredEndpointWhenDockerContextIsCustomFromConfigFile()
+      {
+        // Given
+        using var context = new TempDockerContext("custom", "tcp://127.0.0.1:2375/");
+        var dockerConfig = new DockerConfig(context.ConfigFile);
+
+        // When
+        var currentEndpoint = dockerConfig.GetCurrentEndpoint();
+
+        // Then
+        Assert.Equal(new Uri("tcp://127.0.0.1:2375/"), currentEndpoint);
       }
 
       [Fact]
@@ -64,31 +96,62 @@ namespace DotNet.Testcontainers.Tests.Unit
     public sealed class DockerHostConfigurationTests
     {
       [Fact]
-      public void ReturnsDefaultOsEndpointWhenDockerHostIsEmpty()
+      public void ReturnsActiveEndpointWhenDockerHostIsEmpty()
       {
         // Given
         ICustomConfiguration customConfiguration = new PropertiesFileConfiguration(new[] { "docker.host=" });
-        var dockerConfig = new DockerConfig(new FileInfo("config.json"), customConfiguration);
+        var dockerConfig = new DockerConfig(customConfiguration);
 
         // When
         var currentEndpoint = dockerConfig.GetCurrentEndpoint();
 
         // Then
-        Assert.Equal(TestcontainersSettings.OS.DockerEndpointAuthConfig.Endpoint, currentEndpoint);
+        Assert.Equal(DockerCli.GetCurrentEndpoint(), currentEndpoint);
       }
 
       [Fact]
       public void ReturnsConfiguredEndpointWhenDockerHostIsSet()
       {
         // Given
+        using var context = new TempDockerContext("custom", "");
         ICustomConfiguration customConfiguration = new PropertiesFileConfiguration(new[] { "docker.host=tcp://127.0.0.1:2375/" });
-        var dockerConfig = new DockerConfig(new FileInfo("config.json"), customConfiguration);
+        var dockerConfig = new DockerConfig(context.ConfigFile, customConfiguration);
 
         // When
         var currentEndpoint = dockerConfig.GetCurrentEndpoint();
 
         // Then
         Assert.Equal(new Uri("tcp://127.0.0.1:2375/"), currentEndpoint);
+      }
+    }
+
+    private class TempDockerContext : IDisposable
+    {
+      private readonly DirectoryInfo _directory;
+
+      public TempDockerContext(string contextName, string endpoint, [CallerMemberName] string caller = "")
+      {
+        _directory = new DirectoryInfo(Path.Combine(TestSession.TempDirectoryPath, caller));
+        _directory.Create();
+        ConfigFile = new FileInfo(Path.Combine(_directory.FullName, "config.json"));
+
+        // lang=json
+        var config = $$"""{ "currentContext": "{{contextName}}" }""";
+        File.WriteAllText(ConfigFile.FullName, config);
+
+        // lang=json
+        var meta = $$"""{ "Name": "{{contextName}}", "Metadata": {}, "Endpoints": { "docker": { "Host": "{{endpoint}}", "SkipTLSVerify":false } } }""";
+        var dockerContextHash = Convert.ToHexString(SHA256.HashData(Encoding.UTF8.GetBytes(contextName))).ToLowerInvariant();
+        var metaDirectory = Path.Combine(_directory.FullName, "contexts", "meta", dockerContextHash);
+        Directory.CreateDirectory(metaDirectory);
+        File.WriteAllText(Path.Combine(metaDirectory, "meta.json"), meta);
+      }
+
+      public FileInfo ConfigFile { get; }
+
+      public void Dispose()
+      {
+        _directory.Delete(recursive: true);
       }
     }
   }
