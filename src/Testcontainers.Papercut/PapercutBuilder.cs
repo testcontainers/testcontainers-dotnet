cdd77694cf46b764b1bd83fa134d4c58745d8d11
Liam Wilson <liamwilson127@gmail.com>
2024-09-20 17:04:18 +0100

    Added a wait strategy using http request is succeeded for the health endpoint for Papercut WebUI Service

---
+++ src/Testcontainers.Papercut/PapercutBuilder.cs
@@ -0,0 +1,79 @@
+using System.Net;
+
+namespace Testcontainers.Papercut;
+
+/// <inheritdoc cref="ContainerBuilder{TBuilderEntity, TContainerEntity, TConfigurationEntity}" />
+[PublicAPI]
+public sealed class PapercutBuilder : ContainerBuilder<PapercutBuilder, PapercutContainer, PapercutConfiguration>
+{
+    public const string PapercutImage = "jijiechen/papercut:latest";
+
+    public const ushort HttpPort = 37408;
+
+    public const ushort SmtpPort = 25;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PapercutBuilder" /> class.
+    /// </summary>
+    public PapercutBuilder()
+        : this(new PapercutConfiguration())
+    {
+        DockerResourceConfiguration = Init().DockerResourceConfiguration;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PapercutBuilder" /> class.
+    /// </summary>
+    /// <param name="resourceConfiguration">The Docker resource configuration.</param>
+    private PapercutBuilder(PapercutConfiguration resourceConfiguration)
+        : base(resourceConfiguration)
+    {
+        DockerResourceConfiguration = resourceConfiguration;
+    }
+
+    /// <inheritdoc />
+    protected override PapercutConfiguration DockerResourceConfiguration { get; }
+
+    /// <inheritdoc />
+    public override PapercutContainer Build()
+    {
+        Validate();
+        return new PapercutContainer(DockerResourceConfiguration);
+    }
+
+    /// <inheritdoc />
+    protected override PapercutBuilder Init()
+    {
+        return base.Init()
+            .WithImage(PapercutImage)
+            .WithPortBinding(HttpPort, true)
+            .WithPortBinding(SmtpPort, true)
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilMessageIsLogged("Now listening on"))
+            .WithWaitStrategy(Wait.ForUnixContainer()
+                .UntilHttpRequestIsSucceeded(strategy => 
+                    strategy.ForPath("/health")
+                        .ForPort(HttpPort)
+                        .ForStatusCode(HttpStatusCode.OK)
+                        .ForResponseMessageMatching(async message => await message.Content.ReadAsStringAsync() == "Papercut WebUI server started successfully.")
+                )
+            );
+    }
+
+    /// <inheritdoc />
+    protected override PapercutBuilder Clone(IResourceConfiguration<CreateContainerParameters> resourceConfiguration)
+    {
+        return Merge(DockerResourceConfiguration, new PapercutConfiguration(resourceConfiguration));
+    }
+
+    /// <inheritdoc />
+    protected override PapercutBuilder Clone(IContainerConfiguration resourceConfiguration)
+    {
+        return Merge(DockerResourceConfiguration, new PapercutConfiguration(resourceConfiguration));
+    }
+
+    /// <inheritdoc />
+    protected override PapercutBuilder Merge(PapercutConfiguration oldValue, PapercutConfiguration newValue)
+    {
+        return new PapercutBuilder(new PapercutConfiguration(oldValue, newValue));
+    }
+}
---
+++ tests/Testcontainers.Papercut.Tests/Usings.cs
@@ -0,0 +1,8 @@
+global using System;
+global using System.Net.Http;
+global using System.Net.Mail;
+global using System.Text.Json;
+global using System.Text.Json.Serialization;
+global using System.Threading.Tasks;
+global using DotNet.Testcontainers.Commons;
+global using Xunit;
