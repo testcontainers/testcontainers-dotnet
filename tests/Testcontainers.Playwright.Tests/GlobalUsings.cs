3733a885d6c864aa0cc9ea8ccda0034628f6f464
Ali Mahboubi <ali_mahboubi@outlook.com>
2024-11-05 11:38:18 +0330

    Refactor usings in test files
    Move common usings to GlobalUsings.cs for better maintainability and cleaner code in TestInitializer.cs. This enhances code organization and reduces redundancy.

--- tests/Testcontainers.Playwright.Tests/GlobalUsings.cs
+++ tests/Testcontainers.Playwright.Tests/GlobalUsings.cs
@@ -1,1 +1,3 @@
 global using Xunit;
+global using System;
+global using System.Threading.Tasks;
--- tests/Testcontainers.Playwright.Tests/TestInitializer.cs
+++ tests/Testcontainers.Playwright.Tests/TestInitializer.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Threading.Tasks;
 using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Configurations;
 using DotNet.Testcontainers.Containers;
