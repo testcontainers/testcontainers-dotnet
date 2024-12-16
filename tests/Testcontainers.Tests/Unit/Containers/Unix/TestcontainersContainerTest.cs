2686bca04159ec2fc10f3f4bc67b538839acbc7b
Stefan Nikolei <stefan@nikolei.eu>
2024-12-16 09:45:52 +0100

    Put the test in a different location

--- tests/Testcontainers.Tests/Unit/Containers/Unix/TestcontainersContainerTest.cs
+++ tests/Testcontainers.Tests/Unit/Containers/Unix/TestcontainersContainerTest.cs
@@ -567,26 +567,6 @@ namespace DotNet.Testcontainers.Tests.Unit
         await Assert.ThrowsAnyAsync<Exception>(() => container.StartAsync())
           .ConfigureAwait(true);
       }
-
-      [Fact]
-      public async Task PauseContainer()
-      {
-        await using var container = new ContainerBuilder()
-          .WithImage(CommonImages.Alpine)
-          .WithEntrypoint(CommonCommands.SleepInfinity)
-          .Build();
-
-        await container.StartAsync()
-          .ConfigureAwait(true);
-
-        await container.PauseAsync().ConfigureAwait(true);
-
-        Assert.Equal(TestcontainersStates.Paused, container.State);
-
-        await container.UnpauseAsync().ConfigureAwait(true);
-        
-        Assert.Equal(TestcontainersStates.Running, container.State);
-      }
     }
   }
 }
