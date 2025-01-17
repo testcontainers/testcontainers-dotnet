a8b7b5bcc2ab84611d3e96b7f1ab5107a9159b2b
Ugochukwu Mmaduekwe <ugo4brain@gmail.com>
2025-01-17 13:20:11 +0100

    simplify lowkey vault builder and configuration

--- src/Testcontainers.LowkeyVault/LowkeyVaultConfiguration.cs
+++ src/Testcontainers.LowkeyVault/LowkeyVaultConfiguration.cs
@@ -9,38 +9,17 @@ public sealed class LowkeyVaultConfiguration : ContainerConfiguration
     /// </summary>
     /// <param name="vaultNames">The vault names.</param>
     /// <param name="aliasMap">The vault aliases.</param>
-    /// <param name="importFilePath">The import file path.</param>
     /// <param name="externalConfigFilePath">The external config file path.</param>
-    /// <param name="debug">The debug flag.</param>
-    /// <param name="logicalHost">The host used to replace host placeholder in import template.</param>
-    /// <param name="logicalPort">The port used to replace port placeholder in import template.</param>
     /// <param name="additionalArguments">The additional arguments to be passed via environment variables to the container.</param>
-    /// <param name="keyStoreFilePath">The keyStore (custom Ssl Certificate) file path.</param>
-    /// <param name="keyStorePassword">The keyStore password.</param>
-    /// <param name="keyStoreType">The keyStore type.</param>
     public LowkeyVaultConfiguration(HashSet<string> vaultNames = null,
                                     Dictionary<string, HashSet<string>> aliasMap = null,
-                                    string importFilePath = null,
                                     string externalConfigFilePath = null,
-                                    bool? debug = null,
-                                    string logicalHost = null,
-                                    ushort? logicalPort = null,
-                                    List<string> additionalArguments = null,
-                                    string keyStoreFilePath = null,
-                                    string keyStorePassword = null,
-                                    StoreType? keyStoreType = null)
+                                    List<string> additionalArguments = null)
     {
         VaultNames = vaultNames;
         AliasMap = aliasMap;
-        ImportFilePath = importFilePath;
         ExternalConfigFilePath = externalConfigFilePath;
-        Debug = debug;
-        LogicalHost = logicalHost;
-        LogicalPort = logicalPort;
         AdditionalArguments = additionalArguments;
-        KeyStoreFilePath = keyStoreFilePath;
-        KeyStorePassword = keyStorePassword;
-        KeyStoreType = keyStoreType;
     }
 
     /// <summary>
@@ -83,75 +62,27 @@ public sealed class LowkeyVaultConfiguration : ContainerConfiguration
     {
         VaultNames = BuildConfiguration.Combine(oldValue.VaultNames, newValue.VaultNames);
         AliasMap = BuildConfiguration.Combine(oldValue.AliasMap, newValue.AliasMap);
-        ImportFilePath = BuildConfiguration.Combine(oldValue.ImportFilePath, newValue.ImportFilePath);
         ExternalConfigFilePath = BuildConfiguration.Combine(oldValue.ExternalConfigFilePath, newValue.ExternalConfigFilePath);
-        Debug = BuildConfiguration.Combine(oldValue.Debug, newValue.Debug);
-        LogicalHost = BuildConfiguration.Combine(oldValue.LogicalHost, newValue.LogicalHost);
-        LogicalPort = BuildConfiguration.Combine(oldValue.LogicalPort, newValue.LogicalPort);
         AdditionalArguments = BuildConfiguration.Combine(oldValue.AdditionalArguments, newValue.AdditionalArguments);
-        KeyStoreFilePath = BuildConfiguration.Combine(oldValue.KeyStoreFilePath, newValue.KeyStoreFilePath);
-        KeyStorePassword = BuildConfiguration.Combine(oldValue.KeyStorePassword, newValue.KeyStorePassword);
-        KeyStoreType = BuildConfiguration.Combine(oldValue.KeyStoreType, newValue.KeyStoreType);
     }
 
     /// <summary>
     /// Gets the Vault names.
     /// </summary>
-    public HashSet<string> VaultNames { get; } = [];
+    internal HashSet<string> VaultNames { get; } = [];
 
     /// <summary>
     /// Gets the Alias Map.
     /// </summary>
-    public Dictionary<string, HashSet<string>> AliasMap { get; } = [];
-
-    /// <summary>
-    /// Gets the Import File Path.
-    /// </summary>
-    public string ImportFilePath { get; }
+    internal Dictionary<string, HashSet<string>> AliasMap { get; } = [];
 
     /// <summary>
     /// Gets the External Config File Path.
     /// </summary>
-    public string ExternalConfigFilePath { get; }
-
-    /// <summary>
-    /// Gets Whether to Log Debug.
-    /// </summary>
-    public bool? Debug { get; }
-
-    /// <summary>
-    /// Gets Logical Host.
-    /// </summary>
-    public string LogicalHost { get; }
-
-    /// <summary>
-    /// Gets Logical Port.
-    /// </summary>
-    public ushort? LogicalPort { get; }
+    internal string ExternalConfigFilePath { get; }
 
     /// <summary>
     /// Gets Additional Arguments.
     /// </summary>
-    public List<string> AdditionalArguments { get; } = [];
-
-    /// <summary>
-    /// Gets KeyStore File Path.
-    /// </summary>
-    public string KeyStoreFilePath { get; }
-
-    /// <summary>
-    /// Gets KeyStore Password.
-    /// </summary>
-    public string KeyStorePassword { get; }
-
-    /// <summary>
-    /// Gets KeyStore Type.
-    /// </summary>
-    public StoreType? KeyStoreType { get; }
-}
-
-public enum StoreType
-{
-    JKS,
-    PKCS12
+    internal List<string> AdditionalArguments { get; } = [];
 }
