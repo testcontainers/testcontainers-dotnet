07588270fc87d82f050e0f82f48fb3881a1fbf15
rafek1241 <rafeq1241@gmail.com>
2025-01-18 20:57:32 +0100

    feat: tests to cover custom azurite case

--- tests/Testcontainers.EventHubs.Tests/EventHubsContainerTest.cs
+++ tests/Testcontainers.EventHubs.Tests/EventHubsContainerTest.cs
@@ -5,11 +5,13 @@ public abstract class EventHubsContainerTest : IAsyncLifetime
     private static readonly ConfigurationBuilder ConfigurationBuilder = ConfigurationBuilder.Create()
         .WithEventHub(EventHubName, 2, [EventHubConsumerGroupName]);
     
-    private readonly EventHubsContainer _eventHubsContainer = new EventHubsBuilder()
-        .WithAcceptLicenseAgreement(true)
-        .WithConfigurationBuilder(ConfigurationBuilder)
-        .Build();
+    private readonly EventHubsContainer _eventHubsContainer;
 
+    private EventHubsContainerTest(EventHubsContainer eventHubsContainer)
+    {
+        _eventHubsContainer = eventHubsContainer;
+    }
+    
     private const string EventHubName = "eh-1";
     private const string EventHubConsumerGroupName = "testconsumergroup";
 
@@ -18,6 +20,7 @@ public abstract class EventHubsContainerTest : IAsyncLifetime
 
     [Fact]
     [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+    
     public async Task SendEvents()
     {
         // Given
@@ -33,7 +36,27 @@ public abstract class EventHubsContainerTest : IAsyncLifetime
         // Then
         Assert.Null(thrownExceptionSend);
     }
+
+    [UsedImplicitly]
+    public sealed class EventHubsDefaultConfiguration() : EventHubsContainerTest(new EventHubsBuilder()
+        .WithAcceptLicenseAgreement(true)
+        .WithConfigurationBuilder(ConfigurationBuilder)
+        .Build());
     
     [UsedImplicitly]
-    public sealed class EventHubsDefaultConfiguration : EventHubsContainerTest;
+    public sealed class EventHubsConfigurationWithCustomAzurite() : EventHubsContainerTest(new EventHubsBuilder()
+        .WithAcceptLicenseAgreement(true)
+        .WithConfigurationBuilder(ConfigurationBuilder)
+        .WithAzurite(AzuriteContainer, CustomAlias)
+        .WithNetwork(Network)
+        .Build())
+    {
+        private const string CustomAlias = "custom-alias";
+        private static readonly INetwork Network = new NetworkBuilder().Build();
+        
+        private static readonly AzuriteContainer AzuriteContainer = new AzuriteBuilder()
+            .WithNetwork(Network)
+            .WithNetworkAliases(CustomAlias)
+            .Build();
+    } 
 }
