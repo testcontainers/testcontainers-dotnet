a7bd12859d58b5d34816caf4637c5a24b1c0ce7a
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2025-02-14 15:48:19 +0100

    chore: Add WithAcceptLicenseAgreement(bool) to container builder

--- src/Testcontainers.ServiceBus/Usings.cs
+++ src/Testcontainers.ServiceBus/Usings.cs
@@ -4,7 +4,6 @@ global using System.Linq;
 global using System.Threading;
 global using System.Threading.Tasks;
 global using Docker.DotNet.Models;
-global using DotNet.Testcontainers;
 global using DotNet.Testcontainers.Builders;
 global using DotNet.Testcontainers.Configurations;
 global using DotNet.Testcontainers.Containers;
