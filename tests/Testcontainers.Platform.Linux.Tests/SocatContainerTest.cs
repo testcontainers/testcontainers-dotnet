9d488b6d92e355d98549ad1a52efb34bea531757
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2025-04-03 13:20:03 +0200

    chore: Apply code style

--- tests/Testcontainers.Platform.Linux.Tests/SocatContainerTest.cs
+++ tests/Testcontainers.Platform.Linux.Tests/SocatContainerTest.cs
@@ -1,16 +1,7 @@
-namespace DotNet.Testcontainers.Tests.Unit
-{
-  using System;
-  using System.Net.Http;
-  using System.Threading.Tasks;
-  using DotNet.Testcontainers.Builders;
-  using DotNet.Testcontainers.Commons;
-  using DotNet.Testcontainers.Containers;
-  using DotNet.Testcontainers.Networks;
-  using Xunit;
+namespace Testcontainers.Tests;
 
-  public sealed class SocatContainerTest : IAsyncLifetime
-  {
+public sealed class SocatContainerTest : IAsyncLifetime
+{
     private const string HelloWorldAlias = "hello-world-container";
 
     private readonly INetwork _network;
@@ -21,41 +12,41 @@ namespace DotNet.Testcontainers.Tests.Unit
 
     public SocatContainerTest()
     {
-      _network = new NetworkBuilder()
-        .Build();
+        _network = new NetworkBuilder()
+            .Build();
 
-      _helloWorldContainer = new ContainerBuilder()
-        .WithImage(CommonImages.HelloWorld)
-        .WithNetwork(_network)
-        .WithNetworkAliases(HelloWorldAlias)
-        .Build();
+        _helloWorldContainer = new ContainerBuilder()
+            .WithImage(CommonImages.HelloWorld)
+            .WithNetwork(_network)
+            .WithNetworkAliases(HelloWorldAlias)
+            .Build();
 
-      _socatContainer = new SocatBuilder()
-        .WithNetwork(_network)
-        .WithTarget(8080, HelloWorldAlias)
-        .WithTarget(8081, HelloWorldAlias, 8080)
-        .Build();
+        _socatContainer = new SocatBuilder()
+            .WithNetwork(_network)
+            .WithTarget(8080, HelloWorldAlias)
+            .WithTarget(8081, HelloWorldAlias, 8080)
+            .Build();
     }
 
     public async Task InitializeAsync()
     {
-      await _helloWorldContainer.StartAsync()
-        .ConfigureAwait(false);
+        await _helloWorldContainer.StartAsync()
+            .ConfigureAwait(false);
 
-      await _socatContainer.StartAsync()
-        .ConfigureAwait(false);
+        await _socatContainer.StartAsync()
+            .ConfigureAwait(false);
     }
 
     public async Task DisposeAsync()
     {
-      await _socatContainer.DisposeAsync()
-        .ConfigureAwait(false);
+        await _socatContainer.DisposeAsync()
+            .ConfigureAwait(false);
 
-      await _helloWorldContainer.DisposeAsync()
-        .ConfigureAwait(false);
+        await _helloWorldContainer.DisposeAsync()
+            .ConfigureAwait(false);
 
-      await _network.DisposeAsync()
-        .ConfigureAwait(false);
+        await _network.DisposeAsync()
+            .ConfigureAwait(false);
     }
 
     [Theory]
@@ -63,19 +54,18 @@ namespace DotNet.Testcontainers.Tests.Unit
     [InlineData(8081)]
     public async Task RequestTargetContainer(int containerPort)
     {
-      // Given
-      using var httpClient = new HttpClient();
-      httpClient.BaseAddress = new UriBuilder(Uri.UriSchemeHttp, _socatContainer.Hostname, _socatContainer.GetMappedPublicPort(containerPort)).Uri;
+        // Given
+        using var httpClient = new HttpClient();
+        httpClient.BaseAddress = new UriBuilder(Uri.UriSchemeHttp, _socatContainer.Hostname, _socatContainer.GetMappedPublicPort(containerPort)).Uri;
 
-      // When
-      using var httpResponse = await httpClient.GetAsync("/ping")
-        .ConfigureAwait(true);
+        // When
+        using var httpResponse = await httpClient.GetAsync("/ping")
+            .ConfigureAwait(true);
 
-      var response = await httpResponse.Content.ReadAsStringAsync()
-        .ConfigureAwait(true);
+        var response = await httpResponse.Content.ReadAsStringAsync()
+            .ConfigureAwait(true);
 
-      // Then
-      Assert.Equal("PONG", response);
+        // Then
+        Assert.Equal("PONG", response);
     }
-  }
 }
