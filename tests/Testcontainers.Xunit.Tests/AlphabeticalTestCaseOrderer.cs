1727850b375eede60476bae50501d8726427519b
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2025-04-21 10:29:33 +0200

    chore: Align project config
    Signed-off-by: Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>

--- tests/Testcontainers.Xunit.Tests/AlphabeticalTestCaseOrderer.cs
+++ tests/Testcontainers.Xunit.Tests/AlphabeticalTestCaseOrderer.cs
@@ -3,12 +3,14 @@ namespace Testcontainers.Xunit.Tests;
 public class AlphabeticalTestCaseOrderer : ITestCaseOrderer
 {
 #if XUNIT_V3
-    public IReadOnlyCollection<TTestCase> OrderTestCases<TTestCase>(IReadOnlyCollection<TTestCase> testCases) where TTestCase : notnull, ITestCase
+    public IReadOnlyCollection<TTestCase> OrderTestCases<TTestCase>(IReadOnlyCollection<TTestCase> testCases)
+        where TTestCase : notnull, ITestCase
     {
-        return testCases.OrderBy(testCase => testCase.TestMethod?.MethodName).ToList();
+        return testCases.OrderBy(testCase => testCase.TestMethod?.MethodName).ToImmutableList();
     }
 #else
-    public IEnumerable<TTestCase> OrderTestCases<TTestCase>(IEnumerable<TTestCase> testCases) where TTestCase : ITestCase
+    public IEnumerable<TTestCase> OrderTestCases<TTestCase>(IEnumerable<TTestCase> testCases)
+        where TTestCase : ITestCase
     {
         return testCases.OrderBy(testCase => testCase.TestMethod.Method.Name);
     }
--- tests/Testcontainers.Xunit.Tests/Usings.cs
+++ tests/Testcontainers.Xunit.Tests/Usings.cs
@@ -1,4 +1,5 @@
 global using System.Collections.Generic;
+global using System.Collections.Immutable;
 global using System.Data.Common;
 global using System.Linq;
 global using System.Threading.Tasks;
