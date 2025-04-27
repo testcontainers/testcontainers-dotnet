d0538371d015790c5d28b2011ff1e0b67a3e4610
Matthias Vill <github@cconstruct.de>
2025-04-27 11:06:21 +0200

    chore: Add xUnit.net v3 tests (#1430)
    Co-authored-by: Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>

--- tests/Testcontainers.Xunit.Tests/RedisContainerTest`1.cs
+++ tests/Testcontainers.Xunit.Tests/RedisContainerTest`1.cs
@@ -12,7 +12,11 @@ public sealed partial class RedisContainerTest(ITestOutputHelper testOutputHelpe
 }
 // # --8<-- [end:ConfigureRedisContainer]
 
+#if XUNIT_V3
+[TestCaseOrderer(ordererType: typeof(Testcontainers.Xunit.Tests.AlphabeticalTestCaseOrderer))]
+#else
 [TestCaseOrderer(ordererTypeName: "Testcontainers.Xunit.Tests.AlphabeticalTestCaseOrderer", ordererAssemblyName: "Testcontainers.Xunit.Tests")]
+#endif
 public sealed partial class RedisContainerTest
 {
     [Fact]
