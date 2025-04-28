b9f49c7b1a0e72f8d7f8cd9d0be5b1709ddb88bc
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2025-04-28 15:24:09 +0200

    chore: Update module

--- tests/Testcontainers.Ollama.Tests/OllamaContainerTests.cs
+++ tests/Testcontainers.Ollama.Tests/OllamaContainerTests.cs
@@ -1,42 +1,21 @@
-namespace Testcontainers.Ollama.Tests
-{
-  public class OllamaContainerTests : IAsyncLifetime
-  {
-    private OllamaContainer _ollamaContainer;
+namespace Testcontainers.Ollama;
 
-    public async Task InitializeAsync()
+public sealed class OllamaContainerTest : IAsyncLifetime
+{
+    private readonly OllamaContainer _ollamaContainer = new OllamaBuilder().Build();
+
+    public Task InitializeAsync()
     {
-      _ollamaContainer = new OllamaBuilder()
-        .OllamaConfig(new OllamaConfiguration(OllamaModels.Llama2))
-        .Build();
-      await _ollamaContainer.StartAsync();
-      await _ollamaContainer.Run();
+        return _ollamaContainer.StartAsync();
     }
 
-    public async Task DisposeAsync()
+    public Task DisposeAsync()
     {
-      await _ollamaContainer.DisposeAsync().AsTask();
+        return _ollamaContainer.DisposeAsync().AsTask();
     }
 
     [Fact]
-    [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
-    public async Task OllamaContainerReturnsSuccessful()
+    public async Task TODO()
     {
-      var client = new OllamaApiClient(_ollamaContainer.GetBaseUrl(), _ollamaContainer.ModelName);
-
-      var chatRequest = new ChatRequest() {
-        Model = _ollamaContainer.ModelName,
-        Stream = false,
-        Messages = new List<Message>()
-        {
-          new Message() { Content = "What is a name", Role = ChatRole.User },
-        }
-      };
-
-      // var response = await client.SendChat(chatRequest, stream => { });
-      // response = response.ToList();
-      //
-      // Assert.True(response.Any());
     }
-  }
 }
