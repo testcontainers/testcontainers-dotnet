4788ed67095d7075313b96695865a3b2526d58c5
Cédric Luthi <cedric.luthi@gmail.com>
2025-04-30 19:44:42 +0200

    feat: Add a wait strategy that waits until the ADO.NET database is available (#1401)
    Co-authored-by: Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>

--- tests/Testcontainers.Oracle.Tests/Usings.cs
+++ tests/Testcontainers.Oracle.Tests/Usings.cs
@@ -1,6 +1,8 @@
+global using System;
 global using System.Data;
 global using System.Data.Common;
 global using System.Threading.Tasks;
+global using DotNet.Testcontainers.Builders;
 global using DotNet.Testcontainers.Commons;
 global using JetBrains.Annotations;
 global using Oracle.ManagedDataAccess.Client;
