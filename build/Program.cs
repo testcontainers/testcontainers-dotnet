4e702e86d20d73ed108f8b219ae8197b5e18e092
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2025-05-09 20:49:48 +0200

    fix: Install SonarScanner for .NET again

--- build/Program.cs
+++ build/Program.cs
@@ -5,6 +5,7 @@ public static class Program
     public static int Main(string[] args)
     {
         return new CakeHost()
+            .InstallTool(new Uri("nuget:?package=dotnet-sonarscanner&version=10.1.2"))
             .UseContext<BuildContext>()
             .UseLifetime<BuildLifetime>()
             .Run(args);
