62caa85c9df0e37632fc81caa0b0508d75596292
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2025-08-21 19:19:44 +0200

    fix: Use correct base class UntilWindowsCommandIsCompleted

--- src/Testcontainers/Configurations/WaitStrategies/UntilInternalTcpPortIsAvailableOnWindows.cs
+++ src/Testcontainers/Configurations/WaitStrategies/UntilInternalTcpPortIsAvailableOnWindows.cs
@@ -1,6 +1,6 @@
 namespace DotNet.Testcontainers.Configurations
 {
-  internal class UntilInternalTcpPortIsAvailableOnWindows : UntilUnixCommandIsCompleted
+  internal class UntilInternalTcpPortIsAvailableOnWindows : UntilWindowsCommandIsCompleted
   {
     public UntilInternalTcpPortIsAvailableOnWindows(int containerPort)
       : base($"Exit(-Not((Test-NetConnection -ComputerName 'localhost' -Port {containerPort}).TcpTestSucceeded))")
