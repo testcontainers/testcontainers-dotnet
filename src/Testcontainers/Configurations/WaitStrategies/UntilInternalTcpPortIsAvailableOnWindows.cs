0d70ba1a223b8a43e13819dcb3e7603744f68f37
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2025-08-21 17:32:04 +0200

    chore: Remove BOM

--- src/Testcontainers/Configurations/WaitStrategies/UntilInternalTcpPortIsAvailableOnWindows.cs
+++ src/Testcontainers/Configurations/WaitStrategies/UntilInternalTcpPortIsAvailableOnWindows.cs
@@ -1,4 +1,4 @@
-﻿namespace DotNet.Testcontainers.Configurations
+namespace DotNet.Testcontainers.Configurations
 {
   internal class UntilInternalTcpPortIsAvailableOnWindows : UntilUnixCommandIsCompleted
   {
