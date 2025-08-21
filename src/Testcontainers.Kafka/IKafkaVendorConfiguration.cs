cef4758acad80745b4ce75ef830543377c2ba8b7
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2025-08-21 20:33:47 +0200

    chore: Change Kafka vendor configuration from class to interface

--- src/Testcontainers.Kafka/IKafkaVendorConfiguration.cs
+++ src/Testcontainers.Kafka/IKafkaVendorConfiguration.cs
@@ -3,30 +3,30 @@ namespace Testcontainers.Kafka;
 /// <summary>
 /// Represents the configuration for a Kafka vendor.
 /// </summary>
-internal abstract class KafkaVendorConfiguration
+internal interface IKafkaVendorConfiguration
 {
     /// <summary>
     /// Gets the Kafka vendor.
     /// </summary>
-    public abstract KafkaVendor Vendor { get; }
+    public KafkaVendor Vendor { get; }
 
     /// <summary>
     /// Gets the consensus protocol.
     /// </summary>
-    public abstract ConsensusProtocol ConsensusProtocol { get; }
+    public ConsensusProtocol ConsensusProtocol { get; }
 
     /// <summary>
     /// Determines whether the specified Docker image belongs to this vendor.
     /// </summary>
     /// <param name="image">The Docker image.</param>
     /// <returns><c>true</c> if the image belongs to this vendor; otherwise, <c>false</c>.</returns>
-    public abstract bool IsImageFromVendor(IImage image);
+    public bool IsImageFromVendor(IImage image);
 
     /// <summary>
     /// Validates the resource configuration.
     /// </summary>
     /// <param name="resourceConfiguration">The resource configuration.</param>
-    public abstract void Validate(KafkaConfiguration resourceConfiguration);
+    public void Validate(KafkaConfiguration resourceConfiguration);
 
     /// <summary>
     /// Creates the startup script.
@@ -34,5 +34,5 @@ internal abstract class KafkaVendorConfiguration
     /// <param name="resourceConfiguration">The resource configuration.</param>
     /// <param name="container">The Kafka container.</param>
     /// <returns>The startup script as a string.</returns>
-    public abstract string CreateStartupScript(KafkaConfiguration resourceConfiguration, KafkaContainer container);
+    public string CreateStartupScript(KafkaConfiguration resourceConfiguration, KafkaContainer container);
 }
