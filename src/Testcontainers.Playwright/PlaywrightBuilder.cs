c56984f788b54c5407525bc17029a2b1f43b5fe8
Ali Mahboubi <46527377+alimahboubi@users.noreply.github.com>
2025-11-07 09:24:51 +0400

    feat: Add Playwright module (#1288)
    Co-authored-by: Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>

---
+++ src/Testcontainers.Playwright/PlaywrightBuilder.cs
@@ -0,0 +1,73 @@
+namespace Testcontainers.Playwright;
+
+/// <inheritdoc cref="ContainerBuilder{TBuilderEntity, TContainerEntity, TConfigurationEntity}" />
+[PublicAPI]
+public sealed class PlaywrightBuilder : ContainerBuilder<PlaywrightBuilder, PlaywrightContainer, PlaywrightConfiguration>
+{
+    public const string PlaywrightNetworkAlias = "standalone-container";
+
+    public const string PlaywrightImage = "mcr.microsoft.com/playwright:v1.55.1";
+
+    public const ushort PlaywrightPort = 8080;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PlaywrightBuilder" /> class.
+    /// </summary>
+    public PlaywrightBuilder()
+        : this(new PlaywrightConfiguration())
+    {
+        DockerResourceConfiguration = Init().DockerResourceConfiguration;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PlaywrightBuilder" /> class.
+    /// </summary>
+    /// <param name="resourceConfiguration">The Docker resource configuration.</param>
+    private PlaywrightBuilder(PlaywrightConfiguration resourceConfiguration)
+        : base(resourceConfiguration)
+    {
+        DockerResourceConfiguration = resourceConfiguration;
+    }
+
+    /// <inheritdoc />
+    protected override PlaywrightConfiguration DockerResourceConfiguration { get; }
+
+    /// <inheritdoc />
+    public override PlaywrightContainer Build()
+    {
+        Validate();
+        return new PlaywrightContainer(DockerResourceConfiguration);
+    }
+
+    /// <inheritdoc />
+    protected override PlaywrightBuilder Init()
+    {
+        return base.Init()
+            .WithImage(PlaywrightImage)
+            .WithNetwork(new NetworkBuilder().Build())
+            .WithNetworkAliases(PlaywrightNetworkAlias)
+            .WithPortBinding(PlaywrightPort, true)
+            .WithEntrypoint("/bin/sh", "-c")
+            // Extract the Playwright version from the container at startup.
+            .WithCommand("npx -y playwright@$(sed --quiet 's/.*\\\"driverVersion\\\": *\"\\([^\"]*\\)\".*/\\1/p' ms-playwright/.docker-info) run-server --port " + PlaywrightPort)
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilMessageIsLogged("Listening on ws://localhost:8080/"));
+    }
+
+    /// <inheritdoc />
+    protected override PlaywrightBuilder Clone(IResourceConfiguration<CreateContainerParameters> resourceConfiguration)
+    {
+        return Merge(DockerResourceConfiguration, new PlaywrightConfiguration(resourceConfiguration));
+    }
+
+    /// <inheritdoc />
+    protected override PlaywrightBuilder Clone(IContainerConfiguration resourceConfiguration)
+    {
+        return Merge(DockerResourceConfiguration, new PlaywrightConfiguration(resourceConfiguration));
+    }
+
+    /// <inheritdoc />
+    protected override PlaywrightBuilder Merge(PlaywrightConfiguration oldValue, PlaywrightConfiguration newValue)
+    {
+        return new PlaywrightBuilder(new PlaywrightConfiguration(oldValue, newValue));
+    }
+}
