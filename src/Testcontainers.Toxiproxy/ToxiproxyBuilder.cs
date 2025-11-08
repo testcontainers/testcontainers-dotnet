8617ccf1286a9959d63cb8fa407f5f4a1656b2a5
iltertaha <ilteraktolga@gmail.com>
2025-11-08 13:25:39 +0000

    feat: Adds Toxiproxy module (#1454)
    Co-authored-by: Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>

---
+++ src/Testcontainers.Toxiproxy/ToxiproxyBuilder.cs
@@ -0,0 +1,80 @@
+namespace Testcontainers.Toxiproxy;
+
+/// <inheritdoc cref="ContainerBuilder{TBuilderEntity, TContainerEntity, TConfigurationEntity}" />
+[PublicAPI]
+public sealed class ToxiproxyBuilder : ContainerBuilder<ToxiproxyBuilder, ToxiproxyContainer, ToxiproxyConfiguration>
+{
+    public const string ToxiproxyImage = "ghcr.io/shopify/toxiproxy:2.12.0";
+
+    public const ushort ToxiproxyControlPort = 8474;
+
+    public const ushort FirstProxiedPort = 8666;
+
+    public const ushort LastProxiedPort = 8666 + 32;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ToxiproxyBuilder" /> class.
+    /// </summary>
+    public ToxiproxyBuilder()
+        : this(new ToxiproxyConfiguration())
+    {
+        DockerResourceConfiguration = Init().DockerResourceConfiguration;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ToxiproxyBuilder" /> class.
+    /// </summary>
+    /// <param name="resourceConfiguration">The Docker resource configuration.</param>
+    private ToxiproxyBuilder(ToxiproxyConfiguration resourceConfiguration)
+        : base(resourceConfiguration)
+    {
+        DockerResourceConfiguration = resourceConfiguration;
+    }
+
+    /// <inheritdoc />
+    protected override ToxiproxyConfiguration DockerResourceConfiguration { get; }
+
+    /// <inheritdoc />
+    public override ToxiproxyContainer Build()
+    {
+        Validate();
+        return new ToxiproxyContainer(DockerResourceConfiguration);
+    }
+
+    /// <inheritdoc />
+    protected override ToxiproxyBuilder Init()
+    {
+        const int count = LastProxiedPort - FirstProxiedPort;
+
+        var toxiproxyBuilder = base.Init()
+            .WithImage(ToxiproxyImage)
+            .WithPortBinding(ToxiproxyControlPort, true)
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(request =>
+                request.ForPath("/version").ForPort(ToxiproxyControlPort)));
+
+        // Allows up to 32 ports to be proxied (arbitrary value). The ports are
+        // exposed here, but whether Toxiproxy listens on them is controlled at
+        // runtime when configuring the proxy.
+        return Enumerable.Range(FirstProxiedPort, count)
+            .Aggregate(toxiproxyBuilder, (builder, port) =>
+                builder.WithPortBinding(port, true));
+    }
+
+    /// <inheritdoc />
+    protected override ToxiproxyBuilder Clone(IResourceConfiguration<CreateContainerParameters> resourceConfiguration)
+    {
+        return Merge(DockerResourceConfiguration, new ToxiproxyConfiguration(resourceConfiguration));
+    }
+
+    /// <inheritdoc />
+    protected override ToxiproxyBuilder Clone(IContainerConfiguration resourceConfiguration)
+    {
+        return Merge(DockerResourceConfiguration, new ToxiproxyConfiguration(resourceConfiguration));
+    }
+
+    /// <inheritdoc />
+    protected override ToxiproxyBuilder Merge(ToxiproxyConfiguration oldValue, ToxiproxyConfiguration newValue)
+    {
+        return new ToxiproxyBuilder(new ToxiproxyConfiguration(oldValue, newValue));
+    }
+}
