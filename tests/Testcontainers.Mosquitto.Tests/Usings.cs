b822e61c9c0d3d451dd6de8e28058033045b93f2
Richard Pringle <1317506+EtherZa@users.noreply.github.com>
2025-11-09 17:30:53 +0800

    feat: Add Mosquitto module (#1522)
    Co-authored-by: Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>

---
+++ tests/Testcontainers.Mosquitto.Tests/Usings.cs
@@ -0,0 +1,8 @@
+global using System;
+global using System.IO;
+global using System.Threading.Tasks;
+global using DotNet.Testcontainers.Commons;
+global using JetBrains.Annotations;
+global using MQTTnet;
+global using Testcontainers.Xunit;
+global using Xunit;
