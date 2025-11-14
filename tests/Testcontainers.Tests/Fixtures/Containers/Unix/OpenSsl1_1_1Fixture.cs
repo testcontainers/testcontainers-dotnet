aa40a9310d5639cf9887bb23a54c28ba9fe18011
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2025-11-14 07:34:12 +0100

    fix: Bump dind image version

--- tests/Testcontainers.Tests/Fixtures/Containers/Unix/OpenSsl1_1_1Fixture.cs
+++ tests/Testcontainers.Tests/Fixtures/Containers/Unix/OpenSsl1_1_1Fixture.cs
@@ -5,7 +5,7 @@ namespace DotNet.Testcontainers.Tests.Fixtures
   [UsedImplicitly]
   public sealed class OpenSsl1_1_1Fixture : DockerMTls
   {
-    public OpenSsl1_1_1Fixture() : base("20.10.18")
+    public OpenSsl1_1_1Fixture() : base("29.0.0")
     {
     }
   }
