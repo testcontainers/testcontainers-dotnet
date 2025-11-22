3f9de427b877a4fee394eab354fd98abbee06417
Diego Martin <coderiff@outlook.com>
2025-11-22 09:35:43 +0100

    feat: Add KurrentDb module (#1583)
    Co-authored-by: Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>

---
+++ tests/Testcontainers.KurrentDb.Tests/KurrentDbContainerTest.cs
@@ -0,0 +1,45 @@
+namespace Testcontainers.KurrentDb;
+
+public sealed class KurrentDbContainerTest : IAsyncLifetime
+{
+    private readonly KurrentDbContainer _kurrentDbContainer = new KurrentDbBuilder().Build();
+
+    public async ValueTask InitializeAsync()
+    {
+        await _kurrentDbContainer.StartAsync()
+            .ConfigureAwait(false);
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        return _kurrentDbContainer.DisposeAsync();
+    }
+
+    [Fact]
+    [Trait(nameof(DockerCli.DockerPlatform), nameof(DockerCli.DockerPlatform.Linux))]
+    public async Task ReadStreamReturnsEvent()
+    {
+        // Given
+        const string eventType = "some-event";
+
+        const string streamName = "some-stream";
+
+        var settings = KurrentDBClientSettings.Create(_kurrentDbContainer.GetConnectionString());
+
+        using var client = new KurrentDBClient(settings);
+
+        var eventData = new EventData(Uuid.NewUuid(), eventType, Array.Empty<byte>());
+
+        // When
+        _ = await client.AppendToStreamAsync(streamName, StreamState.NoStream, new[] { eventData }, cancellationToken: TestContext.Current.CancellationToken)
+            .ConfigureAwait(true);
+
+        var resolvedEvents = client.ReadStreamAsync(Direction.Forwards, streamName, StreamPosition.Start, cancellationToken: TestContext.Current.CancellationToken);
+
+        var resolvedEvent = await resolvedEvents.FirstAsync(cancellationToken: TestContext.Current.CancellationToken)
+            .ConfigureAwait(true);
+
+        // Then
+        Assert.Equal(eventType, resolvedEvent.Event.EventType);
+    }
+}
