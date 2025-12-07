bf3ebbbbb4abbcae3809e84c82c0af216a747456
digital88 <digitaldaemon8@gmail.com>
2025-12-07 15:34:39 +0300

    fix GetImageFromDockerfile

--- tests/Testcontainers.Commons/TestSession.cs
+++ tests/Testcontainers.Commons/TestSession.cs
@@ -18,34 +18,25 @@ public static class TestSession
         if (!File.Exists(fullpath)) throw new Exception($"Dockerfile not found at '{fullpath}'.");
         var lines = File.ReadAllLines(fullpath);
         if (lines.Length == 0) throw new Exception($"Dockerfile located at '{fullpath}' is empty.");
-        if (!string.IsNullOrEmpty(stage)) return FindStage(lines, stage);
-        else return FindFirstTag(lines);
+        return FindStageOrAnyTag(lines, stage);
 
-        DockerImage FindFirstTag(IEnumerable<string> lines)
+        DockerImage FindStageOrAnyTag(IEnumerable<string> lines, string stage)
         {
+            bool shouldSearchStage = !string.IsNullOrEmpty(stage);
             foreach (var line in lines)
             {
                 if (string.IsNullOrEmpty(line)) continue;
+                if (line.StartsWith('#')) continue;
+                if (shouldSearchStage && !line.Trim().ToLowerInvariant().EndsWith($"as {stage}")) continue;
                 var split = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
                 if (split.Length < 2) continue;
                 var imageTag = split[1];
                 return new DockerImage(imageTag);
             }
-            throw new Exception($"Failed to find any image tag in Dockerfile located at '{fullpath}'.");
-        }
-
-        DockerImage FindStage(IEnumerable<string> lines, string stage)
-        {
-            foreach (var line in lines)
-            {
-                if (string.IsNullOrEmpty(line)) continue;
-                if (!line.Trim().ToLowerInvariant().EndsWith($"as {stage}")) continue;
-                var split = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-                if (split.Length < 2) continue;
-                var imageTag = split[1];
-                return new DockerImage(imageTag);
-            }
-            throw new Exception($"Failed to find image with stage {stage} in Dockerfile located at '{fullpath}'.");
+            if (shouldSearchStage)
+                throw new Exception($"Failed to find image with stage '{stage}' in Dockerfile located at '{fullpath}'.");
+            else
+                throw new Exception($"Failed to find any image tag in Dockerfile located at '{fullpath}'.");
         }
     }
 }
