59546a1b4bf2337f41b48dd6d6588e7be6e4ec7e
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2025-12-07 15:16:25 +0100

    chore: Add Dockerfile image cache

--- tests/Testcontainers.Commons/TestSession.cs
+++ tests/Testcontainers.Commons/TestSession.cs
@@ -3,7 +3,7 @@ namespace DotNet.Testcontainers.Commons;
 [PublicAPI]
 public static class TestSession
 {
-    private static readonly Regex FromLinePattern = new Regex("^FROM\\s+(?<arg>--\\S+\\s)*(?<image>\\S+).*", RegexOptions.IgnoreCase, TimeSpan.FromSeconds(1));
+    private static readonly ConcurrentDictionary<string, Dictionary<string, string>> Cache = new ConcurrentDictionary<string, Dictionary<string, string>>();
 
     public static readonly string TempDirectoryPath = Path.Combine(Path.GetTempPath(), "testcontainers-tests", Guid.NewGuid().ToString("D"));
 
@@ -14,33 +14,9 @@ public static class TestSession
 
     public static string GetImageFromDockerfile(string relativeFilePath = "Dockerfile", string stage = "")
     {
-        const string imageGroup = "image";
-
         var absoluteFilePath = Path.GetFullPath(relativeFilePath);
 
-        if (!File.Exists(absoluteFilePath))
-        {
-            throw new FileNotFoundException($"Dockerfile '{absoluteFilePath}' not found.");
-        }
-
-        var lines = File.ReadAllLines(absoluteFilePath)
-            .Select(line => line.Trim())
-            .Where(line => !string.IsNullOrEmpty(line))
-            .Where(line => !line.StartsWith('#'))
-            .ToArray();
-
-        var fromMatches = lines
-            .Select(line => FromLinePattern.Match(line))
-            .Where(match => match.Success)
-            .ToArray();
-
-        var stages = fromMatches
-            .Select(match => new
-            {
-                Stage = match.Value.Split(new[] { " AS ", " As ", " aS ", " as " }, StringSplitOptions.RemoveEmptyEntries).Skip(1).DefaultIfEmpty(string.Empty).First(),
-                Image = match.Groups[imageGroup].Value,
-            })
-            .ToDictionary(item => item.Stage, item => item.Image);
+        var stages = Cache.GetOrAdd(absoluteFilePath, filePath => new DockerfileParser(filePath).Parse());
 
         if (stages.TryGetValue(stage, out var image))
         {
--- tests/Testcontainers.Commons/Usings.cs
+++ tests/Testcontainers.Commons/Usings.cs
@@ -1,4 +1,6 @@
 global using System;
+global using System.Collections.Concurrent;
+global using System.Collections.Generic;
 global using System.Diagnostics;
 global using System.IO;
 global using System.Linq;
