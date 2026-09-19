37d5df9a4df5a3cb2dfd9870d1133884d55786f1
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2026-09-19 12:46:09 +0200

    chore: Remove Minio module (#1769)

--- tests/Testcontainers.Minio.Tests/Usings.cs
+++
@@ -1,9 +0,0 @@
-global using System;
-global using System.IO;
-global using System.Net;
-global using System.Threading.Tasks;
-global using Amazon.S3;
-global using Amazon.S3.Model;
-global using DotNet.Testcontainers.Commons;
-global using DotNet.Testcontainers.Configurations;
-global using Xunit;
